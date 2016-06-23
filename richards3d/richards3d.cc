ca7c417fc19b6329af83bc00565193560c75deeb
Daniel Leitner <daniel.leitner@univie.ac.at>
2016-06-23 11:46:50 +0200

    these are the 1d files renamed (work in progress...)

---
+++ richards3d/richards3d.cc
@@ -0,0 +1,44 @@
+
+/*!
+ * \file
+ *
+ * \brief
+ */
+#include <config.h> // from cmake
+
+#include "richardsproblem1d.hh"  // all relevant is in here
+
+#include <dumux/common/start.hh> // start function and processing of input file
+
+/*!
+ * \brief Provides an interface for customizing error messages associated with
+ *        reading in parameters.
+ *
+ * \param progName  The name of the program, that was tried to be started.
+ * \param errorMsg  The error message that was issued by the start function.
+ *                  Comprises the thing that went wrong and a general help message.
+ */
+void usage(const char *progName, const std::string &errorMsg)
+{
+    if (errorMsg.size() > 0) {
+        std::string errorMessageOut = "\nUsage: ";
+                    errorMessageOut += progName;
+                    errorMessageOut += " [options]\n";
+                    errorMessageOut += errorMsg;
+                    errorMessageOut += "\n\nThe list of mandatory options for this program is:\n"
+                                        "\t-TimeManager.TEnd      End of the simulation [s] \n"
+                                        "\t-TimeManager.DtInitial Initial timestep size [s] \n"
+                                        "\t-Grid.File             Name of the file containing the grid \n"
+                                        "\t                       definition in DGF format\n";
+
+        std::cout << errorMessageOut
+                  << "\n";
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // use RichardsBoxProblem1d, or RichardsCCProblem1d
+    typedef TTAG(RichardsBoxProblem1d) PTT;
+    return Dumux::start<PTT>(argc, argv, usage);
+}
