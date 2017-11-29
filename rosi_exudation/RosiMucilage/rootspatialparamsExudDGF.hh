ebf40f015a8a0cc39b7e1b5e8cedcd22ac8a26e3
Cheng75 <chengcumt12@gmail.com>
2017-11-29 01:21:06 -0800

    new file:   rosi_exudation/CMakeLists.txt 	new file:   rosi_exudation/RosiCitrate/CMakeLists.txt 	new file:   rosi_exudation/RosiCitrate/grids/Maize.dgf 	new file:   rosi_exudation/RosiCitrate/grids/RootSysMRI_1times.dgf 	new file:   rosi_exudation/RosiCitrate/grids/lupine.dgf 	new file:   rosi_exudation/RosiCitrate/grids/myGrid.dgf 	new file:   rosi_exudation/RosiCitrate/richardstestspatialparams.hh 	new file:   rosi_exudation/RosiCitrate/rootspatialparamsExudDGF.hh 	new file:   rosi_exudation/RosiCitrate/rootsystem1p2ctestproblem.hh 	new file:   rosi_exudation/RosiCitrate/rootsystemtestspatialparams.hh 	new file:   rosi_exudation/RosiCitrate/rosiRichards2ctestproblem.hh 	new file:   rosi_exudation/RosiCitrate/soilRichards2ctestproblem.hh 	new file:   rosi_exudation/RosiCitrate/test_rosiCitrate.cc 	new file:   rosi_exudation/RosiCitrate/test_rosiCitrate.input 	new file:   rosi_exudation/RosiMucilage/CMakeLists.txt 	new file:   rosi_exudation/RosiMucilage/grids/Maize.dgf 	new file:   rosi_exudation/RosiMucilage/grids/RootSysMRI_1times.dgf 	new file:   rosi_exudation/RosiMucilage/grids/lupine.dgf 	new file:   rosi_exudation/RosiMucilage/grids/myGrid.dgf 	new file:   rosi_exudation/RosiMucilage/grids/vicia.dgf 	new file:   rosi_exudation/RosiMucilage/richardstestspatialparams.hh 	new file:   rosi_exudation/RosiMucilage/rootspatialparamsExudDGF.hh 	new file:   rosi_exudation/RosiMucilage/rootsystem1p2ctestproblem.hh 	new file:   rosi_exudation/RosiMucilage/rootsystemtestspatialparams.hh 	new file:   rosi_exudation/RosiMucilage/rosiRichards2ctestproblem.hh 	new file:   rosi_exudation/RosiMucilage/soilRichards2ctestproblem.hh 	new file:   rosi_exudation/RosiMucilage/test_rosiMucilage.cc 	new file:   rosi_exudation/RosiMucilage/test_rosiMucilage.input

---
+++ rosi_exudation/RosiMucilage/rootspatialparamsExudDGF.hh
@@ -0,0 +1,307 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ *
+ * \ingroup SpatialParameters
+ * \brief The base class for spatial parameters of rootsystem problems
+ * using a fully implicit discretization method.
+ */
+#ifndef DUMUX_ROOTSYSTEM_SPATIAL_PARAMS_DGF_HH
+#define DUMUX_ROOTSYSTEM_SPATIAL_PARAMS_DGF_HH
+
+#include <dumux/common/propertysystem.hh>
+#include <dumux/common/math.hh>
+
+#include <dune/common/fmatrix.hh>
+
+namespace Dumux {
+// forward declaration of property tags
+namespace Properties {
+NEW_PROP_TAG(SpatialParams);
+}
+
+/*!
+ * \ingroup SpatialParameters
+ */
+
+
+/**
+ * \brief The base class for spatial parameters of one-phase problems
+ * using a fully implicit discretization method.
+ */
+template<class TypeTag>
+class RootsystemSpatialParamsDGF
+{
+    typedef typename GET_PROP_TYPE(TypeTag, Scalar) Scalar;
+    typedef typename GET_PROP_TYPE(TypeTag, GridView) GridView;
+    typedef typename GET_PROP_TYPE(TypeTag, SpatialParams) Implementation;
+    typedef typename GET_PROP_TYPE(TypeTag, GridCreator) GridCreator;
+    typedef typename GET_PROP_TYPE(TypeTag, Grid) Grid;
+
+    enum { dimWorld = GridView::dimensionworld };
+    enum { dim = GridView::dimension};
+
+    typedef typename GridView::template Codim<0>::Entity Element;
+    typedef typename GET_PROP_TYPE(TypeTag, FVElementGeometry) FVElementGeometry;
+
+    typedef typename GridView::ctype CoordScalar;
+    typedef Dune::FieldMatrix<Scalar, dimWorld, dimWorld> DimMatrix;
+    typedef Dune::FieldVector<CoordScalar,dimWorld> GlobalPosition;
+    typedef Dune::GridPtr<Grid> GridPointer;
+
+    enum Root {
+        surfaceIdx = 0,
+        radiusIdx = 1,
+        orderIdx = 2,
+        branchIdx = 3,
+        massIdx = 4,
+        axialPermIdx = 5,
+        radialPermIdx = 6,
+        creationTimeIdx = 7
+    };
+
+public:
+    RootsystemSpatialParamsDGF(const GridView &gridView)
+        : gridView_(gridView)
+    {
+        Kx_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, Scalar, SpatialParams, Kx);
+        Kr_ = GET_RUNTIME_PARAM_FROM_GROUP(TypeTag, Scalar, SpatialParams, Kr);
+        //RootSystemAge_ = GET_RUNTIME_PARAM(TypeTag, Scalar, SpatialParams.RootSystemAge);
+    }
+
+    ~RootsystemSpatialParamsDGF()
+    {}
+
+    void setParams()
+    {
+        int numElems = gridView_.size(0);
+        rootParameter_.resize(numElems);
+        RootSystemAge_= 0;
+        for (const auto& element : elements(gridView_))
+        {
+
+            int elemIdx = gridView_.indexSet().index(element);
+            rootParameter_[elemIdx].resize(9);
+
+            Element level0eIt(element);
+            for(int levelIdx = element.level(); levelIdx != 0; levelIdx--)
+                level0eIt = level0eIt.father();
+
+            Scalar rootLength = element.geometry().volume();
+            //Scalar rootLength = GridCreator::parameters(level0eIt)[3];
+            //Scalar rootSurface = GridCreator::parameters(level0eIt)[2]/(1 << element.level());
+            // root radius
+            rootParameter_[elemIdx][Root::radiusIdx] = GridCreator::parameters(level0eIt)[4];
+
+            // root order
+            rootParameter_[elemIdx][Root::orderIdx] =GridCreator::parameters(level0eIt)[0];
+            // root branch  -> count from 0!!
+            rootParameter_[elemIdx][Root::branchIdx] = GridCreator::parameters(level0eIt)[1];
+            // root surface
+            rootParameter_[elemIdx][Root::surfaceIdx] = GridCreator::parameters(level0eIt)[2];
+            // root mass
+            rootParameter_[elemIdx][Root::massIdx] = GridCreator::parameters(level0eIt)[5];
+            // root timeCreated
+            rootParameter_[elemIdx][Root::creationTimeIdx] = GridCreator::parameters(level0eIt)[8];
+
+            if ((int)rootParameter_[elemIdx][Root::orderIdx] == 1){
+                rootParameter_[elemIdx][Root::axialPermIdx] = Kx_; //Kx
+                rootParameter_[elemIdx][Root::radialPermIdx] = Kr_; //Kr
+            }
+            else if  ((int)rootParameter_[elemIdx][Root::orderIdx] == 2){
+                rootParameter_[elemIdx][Root::axialPermIdx] = Kx_;  //Kx
+                rootParameter_[elemIdx][Root::radialPermIdx] = Kr_;  //Kr
+            }
+            else //order >= 3
+                rootParameter_[elemIdx][Root::axialPermIdx] = Kx_; //Kx
+                rootParameter_[elemIdx][Root::radialPermIdx] = Kr_; //Kr
+
+        }
+    }
+
+    /*!
+     * \brief Averages the intrinsic permeability (Scalar).
+     * \param result averaged intrinsic permeability
+     * \param K1 intrinsic permeability of the first node
+     * \param K2 intrinsic permeability of the second node
+     */
+    void meanK(DimMatrix &result,
+               Scalar K1,
+               Scalar K2) const
+    {
+        const Scalar K = Dumux::harmonicMean(K1, K2);
+        for (int i = 0; i < dimWorld; ++i) {
+            for (int j = 0; j < dimWorld; ++j)
+                result[i][j] = 0;
+            result[i][i] = K;
+        }
+    }
+
+    /*!
+     * \brief Averages the intrinsic permeability (Tensor).
+     * \param result averaged intrinsic permeability
+     * \param K1 intrinsic permeability of the first node
+     * \param K2 intrinsic permeability of the second node
+     */
+    void meanK(DimMatrix &result,
+               const DimMatrix &K1,
+               const DimMatrix &K2) const
+    {
+        // entry-wise harmonic mean. this is almost certainly wrong if
+        // you have off-main diagonal entries in your permeabilities!
+        for (int i = 0; i < dimWorld; ++i)
+            for (int j = 0; j < dimWorld; ++j)
+                result[i][j] = harmonicMean(K1[i][j], K2[i][j]);
+    }
+    /*!
+     * \brief Function for defining the root radius.
+     *
+     * \param element The current element
+     * \param fvGeometry The current finite volume geometry of the element
+     * \param scvIdx The index of the sub-control volume.
+     * \return rootRadius
+     */
+    Scalar rootRadius(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::radiusIdx];
+    }
+
+    Scalar radius(int idx) const
+    {
+        return rootParameter_[idx][Root::radiusIdx];
+    }
+
+    Scalar rootSurface(int eIdx) const
+    {
+        return rootParameter_[eIdx][Root::surfaceIdx];
+    }
+
+    Scalar rootSurface(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::surfaceIdx];
+    }
+
+    Scalar rootcreationTime(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::creationTimeIdx];
+    }
+
+    Scalar rootcreationTime(int idx) const
+    {
+        return rootParameter_[idx][Root::creationTimeIdx];;
+    }
+    /*!
+     * \brief Function for defining the axial conductance (K_x).
+     *
+     * \param element The current element
+     * \param fvGeometry The current finite volume geometry of the element
+     * \param scvIdx The index of the sub-control volume.
+     * \return axial conductance (K_x)
+     */
+    Scalar Kx(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::axialPermIdx];
+    }
+
+    /*!
+     * \brief Function for defining the radial conductanctivity (K_r^*).
+     *
+     * \param element The current element
+     * \param fvGeometry The current finite volume geometry of the element
+     * \param scvIdx The index of the sub-control volume.
+     * \return radial conductanctivity (K_r^*)
+     */
+    Scalar Kr(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::radialPermIdx];
+    }
+
+    Scalar Kr(int eIdx) const
+    {
+        return rootParameter_[eIdx][Root::radialPermIdx];
+    }
+
+    /*
+      Doc me!
+    */
+    Scalar rootOrder(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::orderIdx];
+    }
+
+    /*
+      Doc me!
+    */
+    Scalar rootBranch(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::branchIdx];
+    }
+
+    /*
+      Doc me!
+    */
+    Scalar rootMass(const Element &element,
+            const FVElementGeometry &fvGeometry,
+            int scvIdx) const
+    {
+        int elemIdx = gridView_.indexSet().index(element);
+        return rootParameter_[elemIdx][Root::massIdx];
+    }
+
+private:
+
+    std::vector< std::vector<double> > rootParameter_;
+    const GridView gridView_;
+    Scalar Kx_;
+    Scalar Kr_;
+    Scalar RootSystemAge_;
+
+    Implementation &asImp_()
+    { return *static_cast<Implementation*>(this); }
+
+    const Implementation &asImp_() const
+    { return *static_cast<const Implementation*>(this); }
+};
+
+} // namespace Dumux
+
+#endif
+
