785541645d936b81d1cb06ee9e7d77b7cfa69648
Daniel Leitner <daniel.leitner@simwerk.at>
2019-01-22 11:25:07 +0100

    soil swbot, swtop working

--- rosi_benchmarking/soil/python/swtop.py
+++ rosi_benchmarking/soil/python/swtop.py
@@ -1,4 +1,6 @@
-# Bot Scenario Felicien
+# Wet Top Scenario Felicien
+
+# todo check VG paramter
 
 import os
 import matplotlib.pyplot as plt
@@ -11,16 +13,18 @@ os.chdir(path)
 os.chdir("../../../build-cmake/rosi_benchmarking/soil")
 
 # run dumux
-np_ = 1  # number of processors
+np_ = 8  # number of processors
 if np_ == 1:
-    os.system("./richards1d benchmarks_3d/swtop.input")
+    os.system("./richards3d benchmarks_3d/swtop.input")
 else:
-    os.system("mpirun -n " + str(np_) + " ./richards1d benchmarks_3d/swtop.input -Grid.Overlap 0")
+    os.system("mpirun -n " + str(np_) + " ./richards3d benchmarks_3d/swtop.input -Grid.Overlap 0")
 
 # Figure
-s_, p_, z1_ = read1D_vtp_data("swtop-00000.vtp", False)
+s_, p_, z1_ = read1D_vtp_data("swtop-00001.vtp", False)
 h1_ = vg.pa2head(p_)
-plt.plot(h1_, (z1_ - 2) * 100, "r+")
+plt.plot(h1_, (z1_ - 1.26) * 100, "r+")
+plt.xlabel('$\psi$ (cm)')
+plt.ylabel('Depth (cm)')
 
 plt.show()
 
