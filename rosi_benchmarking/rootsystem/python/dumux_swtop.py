651d6861279334d38476906b1d6eafba3d3364f2
Daniel Leitner <daniel.leitner@simwerk.at>
2019-01-31 16:25:27 +0100

    update plot

--- rosi_benchmarking/rootsystem/python/dumux_swtop.py
+++ rosi_benchmarking/rootsystem/python/dumux_swtop.py
@@ -1,7 +1,6 @@
 #
 # Wet top scenario for 3 phenotypes in static soil
 #
-
 import os
 import matplotlib.pyplot as plt
 from vtk_tools import *
@@ -18,26 +17,24 @@ def toHead(pa):  # Pascal (kg/ (m s^2)) to cm pressure head
     return (pa - ref) * 100 / rho / g
 
 
-# go to the right place
-path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(path)
-os.chdir("../../../build-cmake/rosi_benchmarking/rootsystem")
-
 print("wet", toHead(-10000))
 print("dry", toHead(-300000))
 print("a week ", 7 * 24 * 3600)
-
 trans = 5.33 * .75 * .15 / 86400
 maxtrans = 2 * trans
 print("daily rate ", 5.33, "mm/day = ", trans, " kg/s, maximum ", maxtrans)  #
 
-# run dumux
+# Go to the right place
+path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(path)
+os.chdir("../../../build-cmake/rosi_benchmarking/rootsystem")
+
+# Run dumux
 t = time.time()
-os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Reference/RootSys1.dgf")  # mpirun -n 8
-os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Genotype_laterals/RootSys1.dgf -Problem.Name swtop_b")  # mpirun -n 8
-os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Genotype_volume/RootSys1.dgf -Problem.Name swtop_c")  # mpirun -n 8
-elapsed = time.time() - t
-print("elapsed time is ", elapsed)
+# os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Reference/RootSys1.dgf")  # mpirun -n 8
+# os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Genotype_laterals/RootSys1.dgf -Problem.Name swtop_b")  # mpirun -n 8
+# os.system("./rootsystem input/swtop.input -RootSystem.Grid.File grids/RootSys/Genotype_volume/RootSys1.dgf -Problem.Name swtop_c")  # mpirun -n 8
+print("elapsed time is ", time.time() - t)
 
 with open("swtop_actual_transpiration.txt", 'r') as f:
     d = np.loadtxt(f, delimiter = ',')
@@ -46,27 +43,22 @@ with open("swtop_b_actual_transpiration.txt", 'r') as f:
 with open("swtop_c_actual_transpiration.txt", 'r') as f:
     d3 = np.loadtxt(f, delimiter = ',')
 # Format of txt file:
-# time_, lastActualTrans_, lastTrans_, lastMaxTrans_, (sol[0] - pRef_) * 100 / rho_ / g_, trans
-# 0    , 1               , 2         , 3            , 4                                 , 5
+# time_, lastActualTrans_, lastTrans_, lastMaxTrans_, (p - pRef_) * 100 / rho_ / g_, trans
+# 0    , 1               , 2         , 3            , 4                            , 5
 #
-
-# days = 1
-# t_ = np.linspace(0, days, 6 * 24 * days)
-# y_ = np.sin(t_ * 2.*pi - 0.5 * pi) * trans + trans
-# ax1.plot(t_, y_ * (24 * 3600), 'k')
 
 # Plot collar transpiration & pressure
 fig, ax1 = plt.subplots()
 
 ax1.plot(d[:, 0] / (24 * 3600), d[:, 2] * (24 * 3600), 'k')  # potential transpiration
 ax1.plot(d[:, 0] / (24 * 3600), d[:, 1] * (24 * 3600), 'r')  # reference, actual transpiration
-ax1.plot(d[:, 0] / (24 * 3600), d[:, 3] * (24 * 3600), 'r:')
+ax1.plot(d[:, 0] / (24 * 3600), d[:, 3] * (24 * 3600), 'r:')  # reference, maximal transpiration
 ax1.plot(d2[:, 0] / (24 * 3600), d2[:, 1] * (24 * 3600), 'g')  # lateral
 ax1.plot(d2[:, 0] / (24 * 3600), d2[:, 3] * (24 * 3600), 'g:')  # lateral
 ax1.plot(d3[:, 0] / (24 * 3600), d3[:, 1] * (24 * 3600), 'b')  # volume
 ax1.plot(d3[:, 0] / (24 * 3600), d3[:, 1] * (24 * 3600), 'b:')  # volume
 ax1.legend(['Pot trans', 'actual trans P1', 'max trans P1', 'actual trans P2', 'max trans P2', 'actual trans P3', 'max trans P3'], loc = 'upper left')
-ax1.axis((0, days, 0, 1.3))
+ax1.axis((0, d[-1, 0] / (24 * 3600), 0, 1.3))
 ax1.set_xlabel("Time $[d]$")
 ax1.set_ylabel("Transpiration rate $[kg d^{-1}]$")
 
@@ -75,11 +67,7 @@ ax2.plot(d[:, 0] / (24 * 3600), d[:, 4], 'r--')
 ax2.plot(d2[:, 0] / (24 * 3600), d2[:, 4], 'g--')
 ax2.plot(d3[:, 0] / (24 * 3600), d3[:, 4], 'b--')
 ax2.legend(['pressure P1', 'pressure P2', 'pressure P3'], loc = 'upper right')
-
 ax2.set_ylabel("Pressure at root collar (cm)")
-print(np.min(d[:, 4]))
-
-plt.show()
 
 i = np.argmax(d[:, 1] == d[:, 3])
 print("stress after ", d[i, 0] / (24 * 3600), "days")
@@ -88,16 +76,23 @@ print("stress after ", d[i, 0] / (24 * 3600), "days")
 i = np.argmax(d3[:, 1] == d3[:, 3])
 print("stress after ", d[i, 0] / (24 * 3600), "days")
 
+mid1 = round(d.shape[0] / 2)
+mid2 = round(d2.shape[0] / 2)
+mid3 = round(d3.shape[0] / 2)
+print("transpiration during stress", d[mid1, 1], "kg/s at", d[mid1, 4], "cm, max trans", d[mid1, 3])
+print("transpiration during stress", d2[mid2, 1], "kg/s at", d2[mid2, 4], "cm, max trans", d2[mid2, 3])
+print("transpiration during stress", d3[mid3, 1], "kg/s at", d3[mid3, 4], "cm, max trans", d3[mid3, 3])
+p = d[mid1, 4] + 10000
+print(p)
+
+plt.show()
+
 # for 1 hour time step
 # stress after  0.396412037037 days
 # stress after  0.146412037037 days
 # stress after  0.146412037037 days
 
-# # plot
-# p_, z_ = read3D_vtp_data("swtop-00001.vtp", False)
-# h_ = vg.pa2head(p_)
-# plt.plot(p_, z_[:, 2], "r+")  # cell data
-# plt.ylabel("Depth (m)")
-# plt.xlabel("Xylem pressure (cm)")
-# plt.show()
-
+# days = 1
+# t_ = np.linspace(0, days, 6 * 24 * days)
+# y_ = np.sin(t_ * 2.*pi - 0.5 * pi) * trans + trans
+# ax1.plot(t_, y_ * (24 * 3600), 'k')
