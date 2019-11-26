99c1f0b3925c14f6cbec4ba1b793d120eeed6a3a
Daniel Leitner <daniel.leitner@simwerk.at>
2019-11-26 10:06:21 +0100

    renamed folder

---
+++ rosi_benchmarking/soil_richardsnc/richardsnc.cc
@@ -0,0 +1,222 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup RichardsNCTests
+ * \brief Test for the RichardsNC CC model.
+ */
+
+#include <config.h>
+
+#include <ctime>
+#include <iostream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+#include <dune/grid/io/file/dgfparser/dgfexception.hh>
+#include <dune/grid/io/file/vtk.hh>
+#include <dune/istl/io.hh>
+
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/valgrind.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/common/defaultusagemessage.hh>
+
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/porousmediumflow/richards/newtonsolver.hh>
+
+#include <dumux/assembly/fvassembler.hh>
+
+#include <dumux/io/vtkoutputmodule.hh>
+#include <dumux/io/grid/gridmanager.hh>
+
+#include "../soil_richardsnc/richardsncproblem.hh"
+
+/*
+ * here we go again
+ */
+int main(int argc, char** argv) try
+{
+    using namespace Dumux;
+
+    using TypeTag = Properties::TTag::RichardsWellTracerBox; // define the type tag for this problem
+
+    const auto& mpiHelper = Dune::MPIHelper::instance(argc, argv); // initialize MPI, finalize is done automatically on exit
+
+    if (mpiHelper.rank() == 0) { // print dumux start message
+        DumuxMessage::print(/*firstCall=*/true);
+    }
+
+    Parameters::init(argc, argv); // parse command line arguments and input file
+
+    GridManager<GetPropType<TypeTag, Properties::Grid>> gridManager; // try to create a grid (from the given grid file or the input file)
+    gridManager.init("Soil");
+
+    ////////////////////////////////////////////////////////////
+    // run instationary non-linear problem on this grid
+    ////////////////////////////////////////////////////////////
+
+    // we compute on the leaf grid view
+    const auto& leafGridView = gridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    using FVGridGeometry = GetPropType<TypeTag, Properties::FVGridGeometry>;
+    auto fvGridGeometry = std::make_shared<FVGridGeometry>(leafGridView);
+    fvGridGeometry->update();
+
+    // the problem (initial and boundary conditions)
+    using Problem = GetPropType<TypeTag, Properties::Problem>;
+    auto problem = std::make_shared<Problem>(fvGridGeometry);
+    problem->computePointSourceMap(); // enable point sources
+
+    // the solution vector
+    using SolutionVector = GetPropType<TypeTag, Properties::SolutionVector>;
+    SolutionVector x(fvGridGeometry->numDofs());
+    problem->applyInitialSolution(x);
+    auto xOld = x;
+
+    // the grid variables
+    using GridVariables = GetPropType<TypeTag, Properties::GridVariables>;
+    auto gridVariables = std::make_shared<GridVariables>(problem, fvGridGeometry);
+    gridVariables->init(x);
+
+    // get some time loop parameters  & instantiate time loop
+    using Scalar = GetPropType<TypeTag, Properties::Scalar>;
+    const auto tEnd = getParam<Scalar>("TimeLoop.TEnd");
+    std::shared_ptr<CheckPointTimeLoop<Scalar>> timeLoop; // defined in common/timeloop.hh, everything clear, easy to use.
+    if (tEnd > 0) { // dynamic problem
+        const auto maxDt = getParam<Scalar>("TimeLoop.MaxTimeStepSize");
+        auto initialDt = getParam<Scalar>("TimeLoop.DtInitial"); // initial time step
+        timeLoop = std::make_shared<CheckPointTimeLoop<Scalar>>(/*start time*/0., initialDt, tEnd);
+        timeLoop->setMaxTimeStepSize(maxDt);
+        try { // CheckPoints defined
+            std::vector<double> checkPoints = getParam<std::vector<double>>("TimeLoop.CheckTimes");
+            // insert check points
+            for (auto p : checkPoints) { // don't know how to use the setCheckPoint( initializer list )
+                timeLoop->setCheckPoint(p);
+            }
+        } catch(std::exception& e) {
+            std::cout<< "richards.cc: no check times (TimeLoop.CheckTimes) defined in the input file\n";
+        }
+    } else { // static
+    }
+
+    // intialize the vtk output module
+    using IOFields = GetPropType<TypeTag, Properties::IOFields>;
+    VtkOutputModule<GridVariables, SolutionVector> vtkWriter(*gridVariables, x, problem->name());
+    using VelocityOutput = GetPropType<TypeTag, Properties::VelocityOutput>;
+    vtkWriter.addVelocityOutput(std::make_shared<VelocityOutput>(*gridVariables));
+    IOFields::initOutputModule(vtkWriter); // Add model specific output fields
+    vtkWriter.write(0.0);
+
+    // the assembler with time loop for instationary problem
+    using Assembler = FVAssembler<TypeTag, DiffMethod::numeric>; //  FVAssembler (in fvassembler.hh)
+    std::shared_ptr<Assembler> assembler;
+    if (tEnd>0) {
+        assembler = std::make_shared<Assembler>(problem, fvGridGeometry, gridVariables, timeLoop); // dynamic
+    } else {
+        assembler = std::make_shared<Assembler>(problem, fvGridGeometry, gridVariables); // static
+    }
+
+    // the linear solver
+    using LinearSolver = Dumux::ILU0BiCGSTABBackend;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = Dumux::RichardsNewtonSolver<Assembler, LinearSolver>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver);
+
+    if (tEnd>0)  { // dynamic
+
+        timeLoop->start();
+        do {
+            assembler->setPreviousSolution(xOld); // set previous solution for storage evaluations
+
+            nonLinearSolver.solve(x, *timeLoop); // solve the non-linear system with time step control
+
+            // make the new solution the old solution
+            xOld = x;
+            problem->postTimeStep(x, *gridVariables, timeLoop->timeStepSize());
+            gridVariables->advanceTimeStep();
+
+            timeLoop->advanceTimeStep(); // advance to the time loop to the next step
+
+            if ((timeLoop->isCheckPoint()) || (timeLoop->finished())) { // write vtk output (only at check points)
+                vtkWriter.write(timeLoop->time());
+            }
+
+            timeLoop->reportTimeStep(); // report statistics of this time step
+
+            timeLoop->setTimeStepSize(nonLinearSolver.suggestTimeStepSize(timeLoop->timeStepSize())); // set new dt as suggested by the newton solver
+
+            problem->setTime(timeLoop->time()); // pass current time to the problem
+
+        } while (!timeLoop->finished());
+
+        timeLoop->finalize(fvGridGeometry->gridView().comm());
+
+    } else { // static
+
+        assembler->setPreviousSolution(xOld); // set previous solution for storage evaluations
+        nonLinearSolver.solve(x); // solve the non-linear system
+        vtkWriter.write(1);
+
+    }
+
+    timeLoop->finalize(leafGridView.comm());
+
+    ////////////////////////////////////////////////////////////
+    // finalize, print dumux message to say goodbye
+    ////////////////////////////////////////////////////////////
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+
+    return 0;
+}
+
+catch (Dumux::ParameterException &e)
+{
+    std::cerr << std::endl << e << " ---> Abort!" << std::endl;
+    return 1;
+}
+catch (Dune::DGFException & e)
+{
+    std::cerr << "DGF exception thrown (" << e <<
+                 "). Most likely, the DGF file name is wrong "
+                 "or the DGF file is corrupted, "
+                 "e.g. missing hash at end of file or wrong number (dimensions) of entries."
+                 << " ---> Abort!" << std::endl;
+    return 2;
+}
+catch (Dune::Exception &e)
+{
+    std::cerr << "Dune reported error: " << e << " ---> Abort!" << std::endl;
+    return 3;
+}
+catch (...)
+{
+    std::cerr << "Unknown exception thrown! ---> Abort!" << std::endl;
+    return 4;
+}
