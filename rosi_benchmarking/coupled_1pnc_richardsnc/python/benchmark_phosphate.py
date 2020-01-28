40c87255b0db153ab6a81c9e20ac6bbd71762af6
Daniel Leitner <daniel.leitner@simwerk.at>
2020-01-28 16:43:42 +0100

    phosphate gradient to steep

--- rosi_benchmarking/coupled_1pnc_richardsnc/python/benchmark_phosphate.py
+++ rosi_benchmarking/coupled_1pnc_richardsnc/python/benchmark_phosphate.py
@@ -11,14 +11,17 @@ path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
 os.chdir("../../../build-cmake/rosi_benchmarking/coupled_1pnc_richardsnc")
 
-# trans = 1.e-7 * ((2 * 0.02 * math.pi) * 1) * 1.e-6 * 1000. * (24.*3600.)  # cm/s -> kg/day
-# print("Transpiration", trans, "kg/day") # in the input file
+A = (2 * 0.02 * math.pi) * 1  # cm2
+trans = 1.e-7 * 1e-2 * 1000  # 1e-9 [kg/m2/s]
+trans /= 1e4  # kg/cm2/s
+trans = A * trans * (24.*3600.)  # kg/s -> kg/day
+print("Transpiration", trans, "kg/day")  # in the input file
 
-# Plot Comsol solution
-comsol = np.loadtxt("python/gradients.txt", skiprows = 9)
-r_ = comsol[:, 0]
-c_ = comsol[:, 15]
-plt.plot(r_, c_, "g")
+# # Plot Comsol solution
+# comsol = np.loadtxt("python/gradients.txt", skiprows = 9)
+# r_ = comsol[:, 0]
+# c_ = comsol[:, 15]
+# plt.plot(r_, c_, "g")
 
 os.system("./coupled_1p2c input/benchmark_phosphate.input")
 
@@ -30,12 +33,13 @@ print("pressure head", np.min(h1_), np.max(h1_))
 print("concentration", np.min(c_), np.max(c_))
 print("coordinate", np.min(y_), np.max(y_))
 
-# plt.plot(y_, h1_, "r")
-# plt.ylabel("$\psi$ cm pressure head")
+plt.plot(y_, h1_, "r")
+plt.ylabel("$\psi$ cm pressure head")
 
-plt.plot(y_ * 100, c_, "r")  # c_ *rho [kg / m^3] == c_ *1000 [g/cm]
-plt.ylabel("g/cm3")
-plt.xlabel("cm")
-plt.legend(["Comsol", "Dumux"])
-plt.title("Results after 14 days")
+# plt.plot(y_ * 100, c_, "r")  # c_ *rho [kg / m^3] == c_ *1000 [g/cm]
+# plt.ylabel("g/cm3")
+# plt.xlabel("cm")
+# plt.legend(["Comsol", "Dumux"])
+# plt.title("Results after 14 days")
+
 plt.show()
