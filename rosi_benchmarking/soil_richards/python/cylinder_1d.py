1ed943d27b2dbe318e6653f69040959da269170b
magda <m.landl@fz-juelich.de>
2020-04-08 07:16:15 -0700

    correct py

--- rosi_benchmarking/soil_richards/python/cylinder_1d.py
+++ rosi_benchmarking/soil_richards/python/cylinder_1d.py
@@ -21,15 +21,15 @@ os.system("./richards1d_cyl input/cylinder_1d.input")
 
 #read in data
 os.chdir("../../../build-cmake/rosi_benchmarking/soil_richards/python")
-data = np.loadtxt("cylinder_1d_Comsol.txt")
-z_comsol = data[:,0]/100
+data = np.loadtxt("cylinder_1d_Comsol.txt",skiprows=8)
+z_comsol = data[:,0]
 h_comsol = data[:,-1]
 
 #got to the right place and plot
 os.chdir("../../../build-cmake/rosi_benchmarking/soil_richards")
 s_, p_, z_ = read1D_vtp_data("cylinder_1d-00003.vtp")
 h_ = vg.pa2head(p_)
-plt.plot(z_-0.0002,h_,  "b", z_comsol, h_comsol, "r")
+plt.plot((z_-0.0002)/100,h_,  "b", z_comsol, h_comsol, "r")
 #plt.plot(z_,h_,  "b")
 plt.xlabel('distance from the root surface (cm)')
 plt.ylabel('pressure head (cm)')
