4ff5a97d9610f05fa442140127757b5467a5cd44
magda <m.landl@fz-juelich.de>
2020-04-08 23:56:15 -0700

    update

--- rosi_benchmarking/soil_richardsnc/python/cylinder_1d.py
+++ rosi_benchmarking/soil_richardsnc/python/cylinder_1d.py
@@ -19,20 +19,25 @@ os.chdir(path)
 os.chdir("../../../build-cmake/rosi_benchmarking/soil_richardsnc")
 
 # run dumux
-#os.system("./richardsnc1d_cyl input/cylinder_1d.input")
+os.system("./richardsnc1d_cyl input/cylinder_1d.input")
 
 #read in data
 os.chdir("../../../build-cmake/rosi_benchmarking/soil_richardsnc/python")
 data = np.loadtxt("cylinder_1d_Comsol.txt", skiprows=8)
 z_comsol = data[:,0]
-c_comsol = data[:,-1]*95 #molar mass phosphate = 95g/mol
+c_comsol1 = data[:,25] #10 days 
+c_comsol2 = data[:,-1] #20 days
+
 
 # Figure 
-#s_, c_, z_ = read1D_vtp_data("cylinder_1d-00001.vtp", 13)
-#plt.plot(c_, z_ * 100, "r:")
-plt.plot(z_comsol, c_comsol, "r:")
+s_, c_, z_ = read1D_vtp_data("cylinder_1d-00001.vtp", 13)
+plt.plot(c_, (z_ -0.0002)*100, "r:", z_comsol, c_comsol1, "b:")
+plt.plot( z_comsol, c_comsol1, "b:")
+s_, c_, z_ = read1D_vtp_data("cylinder_1d-00002.vtp", 13)
+plt.plot(c_, (z_-0.0002) * 100, "g:", z_comsol, c_comsol2, "k:")
+
 
 plt.xlabel('distance from the root surface (cm)')
 plt.ylabel('phosphate concentration (g/cm³)')
-#plt.legend(["dumux", "comsol"], loc='lower right')
+plt.legend(["dumux, 10 days", "comsol, 10 days", 'dumux, 20 days", "comsol, 20 days"], loc='lower right')
 plt.show()
