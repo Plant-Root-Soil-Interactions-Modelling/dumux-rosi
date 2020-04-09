91982b04f34ba4b82a818c253072eecb28523033
magda <m.landl@fz-juelich.de>
2020-04-09 05:45:07 -0700

    update

--- rosi_benchmarking/soil_richardsnc/python/cylinder_1d_water.py
+++ rosi_benchmarking/soil_richardsnc/python/cylinder_1d_water.py
@@ -18,25 +18,23 @@ os.chdir(path)
 os.chdir("../../../build-cmake/rosi_benchmarking/soil_richardsnc")
 
 # run dumux
-os.system("./richardsnc1d_cyl input/cylinder_1d.input")
+os.system("./richardsnc1d_cyl input/cylinder_1d_water.input")
 
 # plot dumux results
-s_, c_, z_ = read1D_vtp_data("cylinder_1d-00001.vtp", 13)
-plt.plot((z_ - 0.0002) * 100, c_, "b:",)
-s_, c_, z_ = read1D_vtp_data("cylinder_1d-00002.vtp", 13)
-plt.plot((z_ - 0.0002) * 100, c_, "b",)
+s_, p_, z_ = read1D_vtp_data("cylinder_1d_water-00003.vtp")
+h_ = vg.pa2head(p_)
+plt.plot((z_ - 0.0002) * 100, h_, "b",)
 
 # read and plot comsol data
-os.chdir("../../../build-cmake/rosi_benchmarking/soil_richardsnc/python")
-data = np.loadtxt("cylinder_1d_Comsol_P.txt", skiprows=8)
+os.chdir("../../../build-cmake/rosi_benchmarking/soil_richards/python")
+data = np.loadtxt("cylinder_1d_Comsol_water.txt", skiprows=8)
 z_comsol = data[:, 0]
 h_comsol = data[:, 25]
-h_comsol2 =  data[:, -1]
-plt.plot(z_comsol, h_comsol, "r:",z_comsol, h_comsol2, "r")
+plt.plot(z_comsol, h_comsol, "r")
 
 plt.xlabel('distance from the root surface (cm)')
-plt.ylabel('concentration (mol)')
-plt.legend(["dumux, 10d", "dumux, 20d","comsol, 10d","comsol, 20d"], loc='lower right')
+plt.ylabel('pressure head (cm)')
+plt.legend(["dumux", "comsol"], loc='lower right')
 
 plt.show()
 
