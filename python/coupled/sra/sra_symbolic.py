c2441df42b15ddbd38444a72f1eaf26808053a19
Daniel Leitner <daniel.leitner@univie.ac.at>
2020-11-11 10:43:09 +0100

    derivative seems to work

--- python/coupled/sra/sra_symbolic.py
+++ python/coupled/sra/sra_symbolic.py
@@ -10,7 +10,7 @@ wilting_point = 15000
 sand = [0.045, 0.43, 0.15, 3, 1000]
 loam = [0.08, 0.43, 0.04, 1.6, 50]
 clay = [0.1, 0.4, 0.01, 1.1, 10]
-sp = vg.Parameters(sand)
+sp = vg.Parameters(loam)
 
 h = symbols('h')  # soil matric potential h:=Abs(h)
 hh = symbols('hh')  # soil matric potential as integration bound
@@ -30,7 +30,7 @@ ksat = symbols('K')
 water_content = theta_R + (theta_S - theta_R) / pow(1 + pow(alpha * h, n), m)
 effective_saturation = (water_content - theta_R) / (theta_S - theta_R)
 hydraulic_conductivity = ksat * (effective_saturation ** Rational(1, 2)) * ((1 - pow(1 - pow(effective_saturation, 1 / m), m)) ** 2)
-phi = Integral(hydraulic_conductivity, (h, hh, oo))
+phi_integral = Integral(hydraulic_conductivity, (h, hh, oo))
 
 print("water_content             ", water_content)
 print("water_content             ", simplify(water_content))
@@ -38,33 +38,43 @@ print("effective_saturation      ", effective_saturation)
 print("effective_saturation      ", simplify(effective_saturation))
 print("hydraulic_conductivity    ", hydraulic_conductivity)
 print("hydraulic_conductivity    ", simplify(hydraulic_conductivity))
-print("phi                       ", phi)
+print("phi                       ", phi_integral)
 # print("matric_flux_potential     ", simplify(matric_flux_potential))  # generally too hard?
 # print("matric_flux_potential-200 ", matric_flux_potential.evalf(subs = {hh:-200.})) # generally too hard?
 
-# q_out = symbols('q_out')
-q_out = Integer(0)
+q_out = symbols('q_out')
+# q_out = Integer(0)
 r_in = symbols('r_in')
 r_out = symbols('r_out')
 rho = r_out / r_in
-r = symbols('r')  # r = r_in # -> mfp_ = 0 -> h = -15000
+r = symbols('r')  
 phi = symbols("phi")
+
 f = (phi + q_out * r_out * ln(1 / rho)) * ((r ** 2 / r_in ** 2 - 1 + 2 * rho ** 2 * ln(r_in / r)) / (rho ** 2 - 1 + 2 * rho ** 2 * ln(1 / rho))) + q_out * r_out * ln(r / r_in)
 
 dfdr = simplify(diff(f, r))
-
+print()
 print("f                       ", f)
 print("f                       ", simplify(f))
 
-print("df/dr                   ", dfdr)
-print("df/dr                   ", simplify(dfdr.subs(r, r_in)))
 
-# print("matric_potential_mfp   ", matric_potential_mfp)
-# print("i try...")
-# mfp = 6.6e-7
-# matric_potential_mfp = solveset(Eq(matric_flux_potential - mfp, 0), hh, domain = S.Reals)
-#
-#
+print()
+print("df/dr                   ", simplify(dfdr))
+dfdr_r_in = simplify(dfdr.subs(r, r_in))
+print("df/dr, r=r_in          ", simplify(dfdr_r_in))
+dfdr0 = simplify(dfdr_r_in.subs(q_out, Integer(0)))
+print("df/dr, q_out=0          ", simplify(dfdr0))
+
+print()
+h_ = 500 # = -500 cm  
+phi_integral_ = phi_integral.subs({hh: h_, theta_R : sp.theta_R, theta_S : sp.theta_S, alpha : sp.alpha, n : sp.n, ksat : sp.Ksat})
+# print(phi_integral_)
+phi_ = phi_integral_.evalf()
+print("phi(h_)                 ", phi_)
+
+print()
+dfdr_  = dfdr0.evalf(subs = {phi:phi_, r_in : 0.02, r_out : 0.6})
+print("df/dr                   ", dfdr_)
+
 
 # print(latex(water_content))
-
