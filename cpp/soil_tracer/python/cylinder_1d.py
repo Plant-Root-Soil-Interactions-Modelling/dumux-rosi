20d726af3c019f1febcaa0a7b538c639eac976c0
Daniel Leitner <daniel.leitner@univie.ac.at>
2020-11-26 11:52:22 +0100

    soil tracer is not working

--- cpp/soil_tracer/python/cylinder_1d.py
+++ cpp/soil_tracer/python/cylinder_1d.py
@@ -6,6 +6,7 @@
 #
 # D. Leitner, 2020
 #
+""" TODO not working, propably a wrong grid type """
 import sys; sys.path.append("../../../../CPlantBox/src/python_modules/")
 
 import os
@@ -17,10 +18,10 @@ import van_genuchten as vg
 # go to the right place
 path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
-#os.chdir("../../../build-cmake/rosi_benchmarking/soil_tracer")
+os.chdir("../../../build-cmake/cpp/soil_tracer")
 
 # run dumux
-#os.system("./tracer_cyl input/cylinder_1d.input")
+os.system("./test_tracer input/cylinder_1d.input")
 
 #read in data
 os.chdir("../../../cpp/soil_tracer/python")
@@ -29,11 +30,11 @@ z_comsol = data[:,0]
 c_comsol = data[:,-1]
 
 # Figure 
-#c_, z_ = read3D_vtp_data("cylinder_1d-00001.vtp", 13)
-#plt.plot(c_, z_[:,0] * 100, "r:")
+c_, z_ = read3D_vtp_data("cylinder_1d-00001.vtp", 13)
+plt.plot(c_, z_[:,0] * 100, "r:")
 plt.plot(z_comsol, c_comsol, "r:")
 
 plt.xlabel('distance from the root surface (cm)')
 plt.ylabel('phosphate concentration (mol/cm³)')
-#plt.legend(["dumux", "comsol"], loc='lower right')
+plt.legend(["dumux", "comsol"], loc='lower right')
 plt.show()
