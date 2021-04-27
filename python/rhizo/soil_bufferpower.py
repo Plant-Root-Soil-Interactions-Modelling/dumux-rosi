1bee1169ba124dfaa46ec2e345d9a062cb1220ba
DanielLeitner <daniel.leitner@simwerk.at>
2021-04-27 10:08:23 +0200

    benchmarking constant bufer power

--- python/rhizo/soil_bufferpower.py
+++ python/rhizo/soil_bufferpower.py
@@ -11,17 +11,21 @@ from mpi4py import MPI; comm = MPI.COMM_WORLD; rank = comm.Get_rank()
 
 """ 
 Cylindrical 1D model from Bauw et al. 2020
+
+makes buffer_power * fluxes sub plots
 """
 
 N = 1000
 # loam = [0.045, 0.43, 0.04, 1.6, 50]
 loam = [0.03, 0.345, 0.01, 2.5, 28.6]  #  qr, qs, alpha, n, ks
 
-buffer_powers = [0, 200, 600, 1200]
-fluxes = [-0.1, -0.01]
-times = [0, 1, 10, 20, 30]  
+# buffer_powers = [0, 200, 600, 1200]
+buffer_powers = [0, 20, 50, 100]
+fluxes = [-0.01, -0.1, -0.2]
+times = [0, 1.25, 10, 20]
+time_ind = [4, 25, 49]  # comsol time indices 1 (t=0) ... 49 (t=20)
 
-s = [[ [] for _ in range(len(buffer_powers)) ], [[] for _ in range(len(buffer_powers))]]
+s = [[ [] for _ in range(len(buffer_powers)) ], [[] for _ in range(len(buffer_powers))], [[] for _ in range(len(buffer_powers))]]
 
 for i, b in enumerate(buffer_powers):
     for j, f in enumerate(fluxes):
@@ -56,37 +60,77 @@ for i, b in enumerate(buffer_powers):
         s[j][i].setCriticalPressure(-15000)  # cm pressure head
         
         points = s[j][i].getDofCoordinates()
-        
-fig, axes = plt.subplots(len(buffer_powers), len(fluxes))
 
-col = ["r", "g:", "b--", "c", "m:", "y--", ]
+col = ["r", "g", "b", "r:", "g:", "b:", ]
 
+""" DUMUX cacluations """
 maxDt = 0.1
 
+x = [[ [] for _ in range(len(buffer_powers)) ], [[] for _ in range(len(buffer_powers))], [[] for _ in range(len(buffer_powers))]]  # matric potentials
+z = [[ [] for _ in range(len(buffer_powers)) ], [[] for _ in range(len(buffer_powers))], [[] for _ in range(len(buffer_powers))]]  # theta*concentration
+for i, b in enumerate(buffer_powers):
+    for j, f in enumerate(fluxes): 
+        for k, dt in enumerate(np.diff(times)):
+            
+            print("Calculating ", i, j, k)
+            
+            s[j][i].ddt = 1.e-3
+            s[j][i].solve(dt, maxDt)
+            
+            x[j][i].append(s[j][i].getSolutionHead())            
+            y = s[j][i].getSolution(1) * 1.  # [kg/kg] -> 1/1000 [kg/m3] [] -> 1 [g/cm3] # solute concentration            
+            theta = s[j][i].getWaterContent()            
+            y = np.array(y)
+            theta = np.array(theta)
+            z[j][i].append(np.multiply(theta, y))
+
+""" concentration plot """
+print("plotting concentration")
+fig, axes = plt.subplots(len(buffer_powers), len(fluxes))
 for i, b in enumerate(buffer_powers):
     for j, f in enumerate(fluxes):
 
+        data = np.loadtxt("comsol2/concentration{:g}{:g}.txt".format(i, j), skiprows=8,)  
+        z_comsol = data[:, 0]
+
         for k, dt in enumerate(np.diff(times)):
-        
-            print("Calculating ", i, j, k)
-        
-            s[j][i].ddt = 1.e-3
-            s[j][i].solve(dt, maxDt)
-        
-            x = s[j][i].getSolutionHead()
-            y = s[j][i].getSolution(1) * 1.  # [kg/kg] -> 1/1000 [kg/m3] [] -> 1 [g/cm3] # solute concentration
             
-            axes[i, j].plot(points[:], y, col[k % len(col)], label="dumux {:g} days".format(s[j][i].simTime))
+            axes[i, j].plot(points[:], z[j][i][k], col[k % len(col)], label="dumux {:g} days".format(s[j][i].simTime))                
+            axes[i, j].plot(z_comsol + 0.02, data[:, time_ind[k]], col[(k + 3) % len(col)], label="comsol {:g} days".format(times[k + 1]))  # indices = days_ indicdes +1 (radii)
+
+            axes[i, j].set_ylabel('solute concentration times theta (g/cm3)')
+            axes[i, j].set_xlim([0.02, 0.6])
 
             if i == len(buffer_powers) - 1:
                 axes[i, j].set_xlabel('distance from the root axis (cm)')
-            axes[i, j].set_ylabel('solute concentration (g/cm3)')
-            axes[i, j].set_ylim([0, 0.021])
-            axes[i, j].set_xlim([0.02, 0.3])
-            if i == 0:
                 axes[i, j].legend()
                 
             axes[i, j].set_title("buffer power {:g}, root influx {:g} cm/s".format(buffer_powers[i], fluxes[j]))
-
-# plt.tight_layout()
+plt.title("Concentration times water content")
 plt.show()
+
+""" matric potential plot """
+print("plotting concentration")
+fig, axes = plt.subplots(len(buffer_powers), len(fluxes))
+for i, b in enumerate(buffer_powers):
+    for j, f in enumerate(fluxes):
+ 
+        data = np.loadtxt("comsol2/pressure{:g}{:g}.txt".format(i, j), skiprows=8,)  
+        z_comsol = data[:, 0]
+ 
+        for k, dt in enumerate(np.diff(times)):
+             
+            axes[i, j].plot(points[:], x[j][i][k], col[k % len(col)], label="dumux {:g} days".format(s[j][i].simTime))                
+            axes[i, j].plot(z_comsol + 0.02, data[:, time_ind[k]], col[(k + 3) % len(col)], label="comsol {:g} days".format(times[k + 1]))  # indices = days_ indicdes +1 (radii)
+ 
+            axes[i, j].set_ylabel('matric potentail (cm)')
+            # axes[i, j].set_xlim([0.02, 0.6])
+
+            if i == len(buffer_powers) - 1:
+                axes[i, j].set_xlabel('distance from the root axis (cm)')
+                axes[i, j].legend()
+                 
+            axes[i, j].set_title("buffer power {:g}, root influx {:g} cm/s".format(buffer_powers[i], fluxes[j]))
+ 
+plt.show()
+
