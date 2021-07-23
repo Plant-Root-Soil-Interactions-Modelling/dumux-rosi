6eca3059734a5df1468aaa5ea02d64693d147d69
DanielLeitner <daniel.leitner@simwerk.at>
2021-07-23 15:34:38 +0200

    revisions

--- python/rhizo/soil_mai.py
+++ python/rhizo/soil_mai.py
@@ -13,6 +13,17 @@ from mpi4py import MPI; comm = MPI.COMM_WORLD; rank = comm.Get_rank()
 Cylindrical 1D model from Bauw et al. 2020
 """
 
+SMALL_SIZE = 22
+MEDIUM_SIZE = 22
+BIGGER_SIZE = 22        
+plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 s = RichardsWrapper(RichardsNCCylFoam())
 s.initialize()
 
