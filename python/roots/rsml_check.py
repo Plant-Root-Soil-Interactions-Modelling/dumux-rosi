ee1e3f4335d7bf284eb0f8474b774de9cd52c3ff
DanielLeitner <daniel.leitner@simwerk.at>
2021-09-28 15:26:05 +0200

    checked sys conversion from cplantbox

--- python/roots/rsml_check.py
+++ python/roots/rsml_check.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 opens and vtk plots the rsml
 """
 
-file_name = "../../grids/RootSystem8.rsml"
+file_name = "../../grids/RootSys_verysimple.rsml"
 
 """ root problem """
 r = XylemFluxPython(file_name)
