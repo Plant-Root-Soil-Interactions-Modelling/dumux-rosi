778e67ebc7223a1298f601b54d147caa85f3ba9f
Daniel Leitner <daniel.leitner@simwerk.at>
2023-01-31 13:24:01 +0100

    more global SA

--- python/Sensitivity/run_SA.py
+++ python/Sensitivity/run_SA.py
@@ -290,14 +290,16 @@ def local_maize():
 
 def global1_soybean():
     root_type = "soybean"
-    file_name = "global_soybean"
+    file_name = "global_soybean_red"
     enviro_type = 0
     sim_time = 30
 
     if rank == 0:
         theta_ = theta = 85. / 180 * np.pi
-        kx = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
-        kr = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        # kx = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        # kr = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        kx = np.array([10 ** x for x in np.linspace(-2., 0., 25)])
+        kr = np.array([10 ** x for x in np.linspace(-2., 0., 25)])
         write_ranges("results/" + file_name,
                      ["kr", "kx"],
                      [ kx , kr ])
@@ -310,16 +312,16 @@ def global1_soybean():
 
 def global1_maize():
     root_type = "maize"
-    file_name = "global_maize"
+    file_name = "global_maize_inc"
     enviro_type = 0
     sim_time = 30
 
     if rank == 0:
         theta_ = theta = 85. / 180 * np.pi
-        # kx = np.linspace(1., 100, 25)
-        # kr = np.linspace(1., 100, 25)
-        kx = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
-        kr = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        # kx = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        # kr = np.array([10 ** x for x in np.linspace(-1., 1., 25)])
+        kx = np.array([10 ** x for x in np.linspace(0., 2., 25)])  # _inc
+        kr = np.array([10 ** x for x in np.linspace(0., 2., 25)])
         write_ranges("results/" + file_name,
                      ["kr", "kx"],
                      [ kx , kr ])
