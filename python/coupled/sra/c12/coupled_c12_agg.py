e22a117b94e83927ced5604d703c94c6a1c7b456
Daniel Leitner <daniel.leitner@simwerk.at>
2023-02-16 15:37:23 +0100

    upsaling benchmark added

---
+++ python/coupled/sra/c12/coupled_c12_agg.py
@@ -0,0 +1,202 @@
+""" 
+Benchmark C1.2 for a static root system in soil (1D or 3D)
+with the steady rate approach and fixed-point-iteration in HESS paper notation
+"""
+import sys; sys.path.append("../../../modules/"); sys.path.append("../../../../../CPlantBox/");  sys.path.append("../../../../../CPlantBox/src/python_modules")
+sys.path.append("../../../../build-cmake/cpp/python_binding/"); sys.path.append("../../../modules/fv/");
+
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import sparse
+
+# import vtk_plot as vp
+from rhizo_models import plot_transpiration
+from scenario_setup import *
+import aggregated_rs as agg
+
+
+def double_(rsx):
+    rsx2 = np.array([ 0. if i % 2 == 0 else rsx[int(i / 2)] for i in range(0, ns)])
+    return rsx2
+
+# r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("3D", age_dependent = False)
+# name = "results/c12_sra"
+
+# r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("3D", age_dependent = True)
+# name = "results/c12b_sra"
+
+
+r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("1D")
+min_b, max_b, cell_number = get_domain1D()
+name = "results/c12_agg_1d"
+
+""" 
+Initialize aggregated hydraulic model 
+"""
+r = agg.create_aggregated_rs(r, rs_age, min_b, max_b, cell_number)
+nodes = r.rs.nodes
+picker = lambda x, y, z: s.pick([0., 0., z])  # reset mapper, since it is 1D
+r.rs.setSoilGrid(picker)  # maps segment
+
+max_error = 10.
+max_iter = 1000
+
+""" for fixed mapping """
+types = r.rs.subTypes
+outer_r = r.rs.segOuterRadii()
+inner_r = r.rs.radii
+rho_ = np.divide(outer_r, np.array(inner_r))
+ns = len(outer_r)
+mapping = np.array([r.rs.seg2cell[j] for j in range(0, ns)])  # redo mapping
+
+""" Doussan """
+Id = sparse.identity(ns).tocsc()  # identity matrix
+kx_ = np.divide(r.getKx(rs_age), r.rs.segLength())  # / dl
+Kx = sparse.diags(kx_).tocsc()
+# print("Kx", Kx.shape, Kx[0, 0], Kx[1, 1])
+kr_ = np.array(r.getEffKr(rs_age))  # times surface (2 a pi length)
+Kr = sparse.diags(kr_).tocsc()
+# print("Kr", Kr.shape, Kr[0, 0], Kr[1, 1])
+IM = r.get_incidence_matrix().tocsc()
+IMt = IM.transpose().tocsc()
+L = IMt @ Kx @ IM  # Laplacian, Hess Eqn (10)
+L = L[1:, 1:]  # == L_{N-1}
+# print("L", L.shape)
+
+print("invert matrix start")
+
+A_dirichlet = (L + Kr).tocsc()
+Ainv_dirichlet = sparse.linalg.inv(A_dirichlet).todense()  # dense
+
+A_neumann = A_dirichlet
+A_neumann[0, 0] -= kx_[0]
+Ainv_neumann = sparse.linalg.inv(A_neumann).todense()  # dense
+
+C_comp_dirichlet = Kr @ (Id - Ainv_dirichlet @ Kr)  # Neumann, Hess, Eqn (24)
+c_dirichlet = (Kr @ Ainv_dirichlet)[:, 0] * (-kx_[0])  # # Hess (25)
+# print("C_comp_dirichlet", type(C_comp_dirichlet), C_comp_dirichlet.shape)
+# print("c_dirichlet", type(c_dirichlet), c_dirichlet.shape)
+
+C_comp_neumann = Kr @ (Id - Ainv_neumann @ Kr)  # Neumann, Hess, Eqn (32)
+c_neumann = (Kr @ Ainv_neumann)[:, 0]  # Hess (33)
+# print("C_comp_neumann", type(C_comp_neumann), C_comp_neumann.shape)
+# print("c_neumann", type(c_neumann), c_neumann.shape)
+
+print("invert matrix stop")
+
+""" Numerical solution """
+start_time = timeit.default_timer()
+rs_age = np.max(r.get_ages())
+x_, y_, w_, cf = [], [], [], []
+sink1d = []
+sx = s.getSolutionHead()  # inital condition, solverbase.py
+
+N = round(sim_time / dt)
+t = 0.
+
+t_pot = -trans * sinusoidal(0)
+hs = np.transpose(np.array([[sx[mapping[j]][0] for j in range(0, ns)]]))
+for j in range(0, len(nodes) - 1):  # from matric to total
+    hs[j, 0] += nodes[j + 1][2]
+rx = Ainv_dirichlet.dot(Kr.dot(hs)) + Ainv_dirichlet[:, 0] * kx_[0] * wilting_point
+q_dirichlet = -Kr.dot(hs - rx)
+if np.sum(q_dirichlet) < t_pot:
+    rx = Ainv_neumann.dot(Kr.dot(hs)) + Ainv_neumann[:, 0] * t_pot  #   # Hess Eqn (29)
+
+for i in range(0, N):
+
+    t_pot = -trans * sinusoidal(t)  # potential transpiration ...
+    print("t_pot", t_pot)
+
+    hs = np.transpose(np.array([[sx[mapping[j]][0] for j in range(0, ns)]]))
+    for j in range(0, len(nodes) - 1):  # from matric to total
+        hs[j, 0] += nodes[j + 1][2]
+
+    err = 1.e6
+    c = 1
+    rx_old = rx  # in total potential
+    while err > max_error and c < max_iter:  # max_iter = 1000 , err>100 works fine
+
+        """ interpolation """
+        wall_interpolation = timeit.default_timer()
+
+        for j in range(0, len(nodes) - 1):  # from total to matric
+            rx[j, 0] -= nodes[j + 1][2]
+        # print(rx.shape, type(rx), hs.shape, type(hs), inner_kr_.shape, type(inner_kr_), rho_.shape, type(rho_))
+        rsx = soil_root_interface_table(rx, hs, inner_kr_, rho_, sra_table_lookup)
+
+        for j in range(0, len(nodes) - 1):  # from matric to total
+            rsx[j, 0] += nodes[j + 1][2]
+        # print("rsx", rsx.shape, rsx[0], rx[0], hs[0])
+        wall_interpolation = timeit.default_timer() - wall_interpolation
+
+        """ xylem matric potential """
+        wall_xylem = timeit.default_timer()
+
+        rx = Ainv_dirichlet.dot(Kr.dot(rsx)) + Ainv_dirichlet[:, 0] * kx_[0] * wilting_point
+        q_dirichlet = -Kr.dot(rsx - rx)
+        if np.sum(q_dirichlet) <= t_pot:
+            rx = Ainv_neumann.dot(Kr.dot(rsx)) + Ainv_neumann[:, 0] * t_pot  #   # Hess Eqn (29)
+
+        err = np.linalg.norm(rx - rx_old)
+        wall_xylem = timeit.default_timer() - wall_xylem
+        rx_old = rx.copy()
+        c += 1
+
+    if np.sum(q_dirichlet) > t_pot:
+        print("dirichlet", c, err, np.sum(q_dirichlet), t_pot)
+        fluxes = r.sumSegFluxes(q_dirichlet[:, 0])
+    else:
+        q_neumann = -Kr.dot(rsx - rx)
+        print("neumann", c, err, np.sum(q_neumann), t_pot)
+        fluxes = r.sumSegFluxes(q_neumann[:, 0])
+
+    water = s.getWaterVolume()
+    s.setSource(fluxes.copy())  # richards.py
+    s.solve(dt)
+    soil_water = (s.getWaterVolume() - water) / dt
+
+    old_sx = sx.copy()
+    sx = s.getSolutionHead()  # richards.py
+    water = s.getWaterVolume()
+
+    if  i % skip == 0:
+        min_sx = np.min(sx)
+        min_rx = np.min(rx)
+        max_sx = np.max(sx)
+        max_rx = np.max(rx)
+        x_.append(t)
+        sum_flux = 0.
+        for f in fluxes.values():
+            sum_flux += f
+        y_.append(soil_water)  # cm3/day (soil uptake)
+        cf.append(sum_flux)  # cm3/day (root system uptake)
+        n = round(float(i) / float(N) * 100.)
+        print("[" + ''.join(["*"]) * n + ''.join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n"
+              .format(min_sx, max_sx, min_rx, max_rx, s.simTime, rx[0, 0]))
+
+        # """ Additional sink plot """
+        # if i % 60 == 0:  # every 6h
+        #     ana = pb.SegmentAnalyser(r.rs)
+        #     fluxes = r.segFluxes(rs_age + t, rx, old_sx, False, cells = True)  # cm3/day WRONG sx, should be old sx
+        #     ana.addData("fluxes", fluxes)  # cut off for vizualisation
+        #     flux1d = ana.distribution("fluxes", max_b[2], min_b[2], 15, False)
+        #     sink1d.append(np.array(flux1d))
+        #     print("\nSink integral = ", np.sum(np.array(flux1d)), "\n")
+
+    t += dt
+
+s.writeDumuxVTK(name)
+
+""" Plot """
+print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
+plot_transpiration(x_, y_, cf, lambda t: trans * sinusoidal(t))
+np.savetxt(name, np.vstack((x_, -np.array(y_))), delimiter = ';')
+
+# vp.plot_roots_and_soil(r.rs, "pressure head", rx, s, False, min_b, max_b, cell_number, name)  # VTK vizualisation
+# sink1d = np.array(sink1d)
+# np.save("sink1d", sink1d)
+# print(sink1d.shape)
+# print(sink1d[-1])
+
---
+++ python/coupled/sra/c12/coupled_c12_up.py
@@ -0,0 +1,207 @@
+""" 
+Benchmark C1.2 for a static root system in soil (1D or 3D)
+with upscaled C_comp matrix using Doussan approach in HESS paper notation
+"""
+import sys; sys.path.append("../../../modules/"); sys.path.append("../../../../../CPlantBox/");  sys.path.append("../../../../../CPlantBox/src/python_modules")
+sys.path.append("../../../../build-cmake/cpp/python_binding/"); sys.path.append("../../../modules/fv/");
+
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import sparse
+
+# import vtk_plot as vp
+from rhizo_models import plot_transpiration
+from scenario_setup import *
+
+r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("3D")
+name = "results/c12_up"
+
+# r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("3D", age_dependent = True)
+# dt = 36 / (24 * 3600)  # unstable otherwise
+# name = "results/c12b_up"
+
+# r, rs_age, trans, wilting_point, soil_, s, sra_table_lookup, mapping, sim_time, dt, skip = set_scenario("1D")
+# name = "results/c12_up1d"
+
+nodes = r.get_nodes()
+ns = len(r.rs.segments)
+
+""" Doussan """
+Id = sparse.identity(ns).tocsc()  # identity matrix
+kx_ = np.divide(r.getKx(rs_age), r.rs.segLength())  # / dl
+Kx = sparse.diags(kx_).tocsc()
+# print("Kx", Kx.shape, Kx[0, 0], Kx[1, 1])
+kr_ = np.array(r.getEffKr(rs_age))  # times surface (2 a pi length)
+Kr = sparse.diags(kr_).tocsc()
+# print("Kr", Kr.shape, Kr[0, 0], Kr[1, 1])
+IM = r.get_incidence_matrix().tocsc()
+IMt = IM.transpose().tocsc()
+L = IMt @ Kx @ IM  # Laplacian, Hess Eqn (10)
+L = L[1:, 1:]  # == L_{N-1}
+# print("L", L.shape)
+
+print("invert matrix start")
+
+A_dirichlet = (L + Kr).tocsc()
+Ainv_dirichlet = sparse.linalg.inv(A_dirichlet).todense()  # dense
+
+A_neumann = A_dirichlet
+A_neumann[0, 0] -= kx_[0]
+Ainv_neumann = sparse.linalg.inv(A_neumann).todense()  # dense
+
+C_comp_dirichlet = Kr @ (Id - Ainv_dirichlet @ Kr)  # Neumann, Hess, Eqn (24)
+c_dirichlet = (Kr @ Ainv_dirichlet)[:, 0] * (-kx_[0])  # # Hess (25)
+# print("C_comp_dirichlet", type(C_comp_dirichlet), C_comp_dirichlet.shape)
+# print("c_dirichlet", type(c_dirichlet), c_dirichlet.shape)
+C_comp_neumann = Kr @ (Id - Ainv_neumann @ Kr)  # Neumann, Hess, Eqn (32)
+c_neumann = (Kr @ Ainv_neumann)[:, 0]  # Hess (33)
+# print("C_comp_neumann", type(C_comp_neumann), C_comp_neumann.shape)
+# print("c_neumann", type(c_neumann), c_neumann.shape)
+
+print("invert matrix stop")
+
+print("upscaling start")
+
+B, soil2matrix, matrix2soil = r.get_soil_matrix()
+nmax = len(matrix2soil)  # dimension of the upsaled problem
+
+Bt = B.transpose()
+# print(Bt.shape, C_comp_neumann.shape, B.shape)
+# print(np.sum(Bt @ B), (Bt @ B).shape)
+BBt_inv = sparse.linalg.inv(B @ Bt)  # sparse
+
+C_comp_neumann_up = B @ C_comp_neumann @ Bt
+c_neumann_up = B @ c_neumann
+C_comp_dirichlet_up = B @ C_comp_dirichlet @ Bt
+c_dirichlet_up = B @ c_dirichlet
+# print(C_comp_neumann_up.shape, type(C_comp_neumann_up))
+
+print("upscaling end")
+
+""" Numerical solution"""
+start_time = timeit.default_timer()
+rs_age = np.max(r.get_ages())
+x_, y_, w_, cf = [], [], [], []
+sink1d = []
+
+N = round(sim_time / dt)
+t = 0.
+
+rx = [0]
+
+sx = s.getSolutionHead()  # inital condition, solverbase.py
+centers = s.getCellCenters()
+
+for i in range(0, N):
+
+    t_pot = -trans * sinusoidal(t)  # potential transpiration ...
+    print("t_pot", t_pot)
+
+    # hs = np.transpose(np.array([[sx[mapping[j]][0] for j in range(0, ns)]]))
+    # for j in range(0, len(nodes) - 1):  # from matric to total
+    #     hs[j, 0] += nodes[j + 1][2]
+    #
+    # q_dirichlet0 = -(C_comp_dirichlet.dot(hs) + c_dirichlet * wilting_point)
+    #
+    # if np.sum(q_dirichlet0) > t_pot:
+    #     print("dirichlet", np.sum(q_dirichlet0), t_pot)
+    #     fluxes = r.sumSegFluxes(q_dirichlet0[:, 0])
+    # else:
+    #     q_neumann0 = -(C_comp_neumann.dot(hs) - c_neumann * t_pot)
+    #     print("neumann", np.sum(q_neumann0), t_pot)
+    #     fluxes = r.sumSegFluxes(q_neumann0[:, 0])
+
+    hs_ = np.zeros((nmax, 1))
+    for j in soil2matrix.keys():
+            hs_[soil2matrix[j]] += sx[j] + centers[j, 2]
+
+    # hs = np.transpose(np.array([[sx[mapping[j]][0] for j in range(0, ns)]]))
+    # for j in range(0, len(nodes) - 1):  # from matric to total
+    #     hs[j, 0] += nodes[j + 1][2]
+    # hs_ = BBt_inv.dot(B.dot(hs))  # can be proven using pseudo inverse
+
+    q_dirichlet0_up = -(C_comp_dirichlet_up.dot(hs_) + c_dirichlet_up * wilting_point)
+
+    if np.sum(q_dirichlet0_up) > t_pot:
+        print("q_dirichlet0_up", np.sum(q_dirichlet0_up), t_pot)
+        fluxes = {}
+        for j in range(0, nmax):
+            fluxes[matrix2soil[j]] = q_dirichlet0_up[j, 0]
+    else:
+        # print("C_comp_neumann_up", C_comp_neumann_up.shape)
+        # print("c_neumann_up", c_neumann_up.shape)
+        # print("hs_", hs_.shape)
+        q_neumann0_up = -(C_comp_neumann_up.dot(hs_) - c_neumann_up * t_pot)
+        print("q_neumann0_up", q_neumann0_up.shape, np.sum(q_neumann0_up), t_pot)
+        fluxes = {}
+        for j in range(0, nmax):
+            fluxes[matrix2soil[j]] = q_neumann0_up[j, 0]
+
+    # print()
+    # hx = Ainv_neumann.dot(Kr.dot(hs)) + Ainv_neumann[:, 0] * t_pot  #   # Hess Eqn (29)
+    # hx0 = hx[0, 0] + t_pot / kx_[0]  # /kx*l
+    # print("hx0", hx0, hx[0, 0], hx[1, 0], hx[2, 0])
+    # hxd = Ainv_dirichlet.dot(Kr.dot(hs)) + Ainv_dirichlet[:, 0] * kx_[0] * wilting_point
+    # print("hxd", hxd[0, 0], hxd[1, 0], hxd[2, 0])
+    # print()
+    #
+    # print()
+    # q_neumann = -Kr.dot(hs - hx)
+    # print("q_neumann", q_neumann.shape, np.min(q_neumann), np.max(q_neumann), np.argmin(q_neumann), np.sum(q_neumann))
+    # print("q_neumann0", q_neumann.shape, np.min(q_neumann0), np.max(q_neumann0), np.argmin(q_neumann0), np.sum(q_neumann0))
+    # q_dirichlet = -Kr.dot(hs - hxd)
+    # print("q_dirichlet", q_dirichlet.shape, np.min(q_dirichlet), np.max(q_dirichlet), np.argmin(q_dirichlet), np.sum(q_dirichlet))
+    # print("q_dirichlet", q_dirichlet0.shape, np.min(q_dirichlet0), np.max(q_dirichlet0), np.argmin(q_dirichlet0), np.sum(q_dirichlet0))
+    # # print("sum_q", np.sum(q), q[0], q[1])
+    # # hx = [0]
+    # print()
+
+    water = s.getWaterVolume()
+    s.setSource(fluxes.copy())  # richards.py
+    s.solve(dt)
+    soil_water = (s.getWaterVolume() - water) / dt
+
+    old_sx = sx.copy()
+    sx = s.getSolutionHead()  # richards.py
+
+    if  i % skip == 0:
+        min_sx = np.min(sx)
+        min_rx = np.min(rx)
+        max_sx = np.max(sx)
+        max_rx = np.max(rx)
+        x_.append(t)
+        sum_flux = 0.
+        for f in fluxes.values():
+            sum_flux += f
+        # print("sum_flux", sum_flux.shape, sum_flux)
+        y_.append(soil_water)  # cm3/day
+        cf.append(sum_flux)  # cm3/day
+        n = round(float(i) / float(N) * 100.)
+        print("[" + ''.join(["*"]) * n + ''.join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n"
+              .format(min_sx, max_sx, min_rx, max_rx, s.simTime, rx[0]))
+
+        # """ Additional sink plot """
+        # if i % 60 == 0:  # every 6h
+        #     ana = pb.SegmentAnalyser(r.rs)
+        #     fluxes = r.segFluxes(rs_age + t, rx, old_sx, False, cells = True)  # cm3/day WRONG sx, should be old sx
+        #     ana.addData("fluxes", fluxes)  # cut off for vizualisation
+        #     flux1d = ana.distribution("fluxes", max_b[2], min_b[2], 15, False)
+        #     sink1d.append(np.array(flux1d))
+        #     print("\nSink integral = ", np.sum(np.array(flux1d)), "\n")
+
+    t += dt
+
+s.writeDumuxVTK(name)
+
+""" Plot """
+print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
+plot_transpiration(x_, y_, cf, lambda t: trans * sinusoidal(t))
+np.savetxt(name, np.vstack((x_, -np.array(y_))), delimiter = ';')
+
+# vp.plot_roots_and_soil(r.rs, "pressure head", rx, s, False, min_b, max_b, cell_number, name)  # VTK vizualisation
+# sink1d = np.array(sink1d)
+# np.save("sink1d", sink1d)
+# print(sink1d.shape)
+# print(sink1d[-1])
+
