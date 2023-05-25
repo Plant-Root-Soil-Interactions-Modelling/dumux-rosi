5f17cf4c56eb164aee0f02191cb560d6207cc054
Daniel Leitner <d.leitner@fz-juelich.de>
2023-05-25 13:17:24 +0200

    Update job.sh

--- python/coupled/upscaling/jobs/job.sh
+++ python/coupled/upscaling/jobs/job.sh
@@ -7,7 +7,7 @@
 #SBATCH --exclude=node02
 #SBATCH --partition=cpu256
 #SBATCH --time=48:00:00
-#SBATCH --mem=0
+#SBATCH --mem=10G
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=d.leitner@fz-juelich.de
 cd ..
