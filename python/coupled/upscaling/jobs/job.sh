0b3058e021a7a11bb52f9de18a573a9749eba07d
Daniel Leitner <d.leitner@fz-juelich.de>
2023-05-31 11:25:19 +0200

    12 jobs

--- python/coupled/upscaling/jobs/job.sh
+++ python/coupled/upscaling/jobs/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                                                                                
 #                                                                                                                                                          
 #SBATCH --job-name=upscaling_scenarios
-#SBATCH --ntasks=128
+#SBATCH --ntasks=12
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --exclude=node02
@@ -12,4 +12,4 @@
 #SBATCH --mail-user=d.leitner@fz-juelich.de
 cd ..
 module load openmpi/4.1.4
-mpirun -n 48 python3 jobs.py
+mpirun -n 12 python3 jobs.py
