47d8feae1d8569904ac731b77a3b52df852b3940
Daniel Leitner <daniel.leitner@univie.ac.at>
2023-07-28 11:17:51 +0200

    clusterific

--- python/coupled/upscaling/jobs.py
+++ python/coupled/upscaling/jobs.py
@@ -12,6 +12,8 @@ import scenario_sra as sra
 import scenario_sra_old as sra_old
 import scenario_agg as agg
 
+import matplotlib
+matplotlib.use('Agg')
 
 def start_jobs(jobs):
     """ send as individual jobs """
