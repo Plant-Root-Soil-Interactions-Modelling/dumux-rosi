fix theta computation

python/modules_fpit/.ipynb_checkpoints/rhizo_modelsPlant-checkpoint.py
@@ -470,9 +470,11 @@ class RhizoMappedSegments(pb.MappedPlant):#XylemFluxPython):#
         XX_leftover = np.full(theta_leftOver.shape, np.nan)
         if len(theta_leftOver) > 0:#[correctTheta]
             try:
-                #print('get_XX_leftover_B', rank,np.array([tlo for tlo in theta_leftOver[correctTheta]]))
                 # XX_leftover[correctTheta] = np.array([vg.pressure_head( tlo, self.vg_soil) for tlo in theta_leftOver[correctTheta]])
-                XX_leftover = np.array([vg.pressure_head( tlo, self.vg_soil) for tlo in theta_leftOver ])
+                idtocompute = np.where(~np.isnan(theta_leftOver))
+                #print('get_XX_leftover_B', rank,theta_leftOver, theta_leftOver.shape, idtocompute)
+                XX_leftover[idtocompute] = np.array([vg.pressure_head( tlo, self.vg_soil) for tlo in theta_leftOver[idtocompute]])
+                #print('get_XX_leftover_C',XX_leftover)
             except:
                 print(theta_leftOver[lowTheta],volLeftOver[lowTheta])
                 print(theta_leftOver[highTheta],volLeftOver[highTheta])
@@ -1402,6 +1404,8 @@ class RhizoMappedSegments(pb.MappedPlant):#XylemFluxPython):#
                 gradientNew = self.interAndExtraPolation(points[1:-1],oldPoints[1:-1], gradientOld)
                 
                 wOld = sum(theta_old*volOld)
+                maxVal = self.vg_soil.theta_S - 1e-5 # keep that or set lower higher bound?
+                minVal = self.theta_wilting_point
                 changeRatioW = max(min(changeRatio, sum(maxVal*volNew)/wOld),sum(minVal*volNew)/wOld)
                 try:
                     assert ((changeRatioW <= 1.) and (changeRatioW > 0.))
@@ -1414,6 +1418,9 @@ class RhizoMappedSegments(pb.MappedPlant):#XylemFluxPython):#
                     raise Exception
                 try:
                     theta_new = self.updateConcentration(totContent = wOld*changeRatioW,changeRatio=changeRatioW, gradient =gradientNew, theta_old_ = theta_old, volumes = volNew,isWater = True)
+                    #newHead = np.full(theta_new.shape, np.nan)
+                    #idtocompute = np.where(~np.isnan(theta_leftOver))
+                    #newHead[idtocompute] = np.array([vg.pressure_head(nt, self.vg_soil) for nt in theta_new[idtocompute]])# cm
                     newHead = np.array([vg.pressure_head(nt, self.vg_soil) for nt in theta_new])# cm
                 except:
                     print('issue updateConcentration, gradientNew',gradientNew,points[1:-1],oldPoints[1:-1], gradientOld )
@@ -1626,9 +1633,12 @@ class RhizoMappedSegments(pb.MappedPlant):#XylemFluxPython):#
             try:
                 self.cyls.append(self.initialize_dumux_nc_(gId, x[self.seg2cell[gId]], cc[self.seg2cell[gId]]))
             except:
-                print('error at initialization',gId,self.seg2cell[gId], self.organTypes[gId],
+                print('error at initialization',gId,self.seg2cell[gId],
+                      self.organTypes[gId],
                     self.radii[gId], self.outer_radii[gId],self.seg_length[gId],
-                    x[self.seg2cell[gId]], cc[self.seg2cell[gId]] )
+                    x[self.seg2cell[gId]],
+                      cc[self.seg2cell[gId]],self.seg2cell[gId] )
+                write_file_float('xErrInit', x, directory_ =self.results_dir )
                 raise Exception
         else:
             a_in = self.radii[gId]#get Perimeter instead? not used for now anyway

python/paperSc/XcGrowth.py
@@ -309,8 +309,36 @@ def XcGrowth(initsim, mode,simMax,extraName,paramIndx_,spellData):
                         write_file_array("fpit_sol_content1d"+str(nc+1), rs.getContentCyl(idComp = nc+1, doSum = False, reOrder = True), 
                                          directory_ =results_dir, fileType = '.csv')  
             start = False
-        if (rank == 0) and (not doMinimumPrint):
+        if (rank == 0):# and (not doMinimumPrint):
             write_file_array("organTypes", np.array(rs.organTypes), directory_ =results_dir)
+            write_file_array("subTypes", np.array(r.rs.subTypes), directory_ =results_dir)
+            length_Segs = np.array(r.rs.segLength())
+            write_file_array("length_Segs", length_Segs, directory_ =results_dir)
+            orgs = r.plant.getOrgans(-1, False)
+            parentorgid = np.array([org.getParent().getId() for org in orgs])
+            parentNidx = np.array([org.getParent().getNodeId(org.parentNI) for org in orgs])
+            id_orgs = np.array([org.getId() for org in orgs])
+            ot_orgs = np.array([org.organType() for org in orgs])
+            st_orgs = np.array([org.getParameter("subType") for org in orgs])
+            lenOrg = np.array([org.getLength(False) for org in orgs])   
+            write_file_array("id_orgs", id_orgs, directory_ =results_dir)
+            write_file_array("ot_orgs", ot_orgs, directory_ =results_dir)
+            write_file_array("st_orgs", st_orgs, directory_ =results_dir)  
+            write_file_array("lenOrg", lenOrg, directory_ =results_dir) 
+            write_file_array("parentNidx", parentNidx, directory_ =results_dir) 
+            
+            write_file_array("nodes_X",
+                             np.array([tempnode[0] for tempnode in r.get_nodes()]), 
+                             directory_ =results_dir)
+            write_file_array("nodes_Y", np.array([tempnode[1] for tempnode in r.get_nodes()]), directory_ =results_dir)
+            write_file_array("nodes_Z", np.array([tempnode[2] for tempnode in r.get_nodes()]), directory_ =results_dir)
+            idPerNode = np.concatenate([
+                np.full(org.getNumberOfNodes(),org.getId()) for org in orgs]).reshape(-1)
+            globalNodeId = np.concatenate([org.getNodeIds() for org in orgs]).reshape(-1)
+            write_file_array("orgidPerNode", idPerNode, directory_ =results_dir)
+            write_file_array("globalNodeId", globalNodeId, directory_ =results_dir)
+
+            
         
                            
 
@@ -828,7 +856,8 @@ def XcGrowth(initsim, mode,simMax,extraName,paramIndx_,spellData):
 if __name__ == '__main__':
     # python3 XcGrowth.py 9 dumux_10c 10 0 customDry noAds 9.02 0.02
     # python3 XcGrowth.py 9 dumux_10c 10 1640 lateDry
-    print('sys.argv',sys.argv)
+    if rank == 0:
+        print('sys.argv',sys.argv)
     initsim =float(sys.argv[1])# initsim = 9.5
     mode = sys.argv[2] #"dumux_w" "dumux_3c" "dumux_10c" 
     

python/paperSc/cyl3plant.py
@@ -747,9 +747,11 @@ def simulate_const(s, rs, sim_time, dt, rs_age, Q_plant,
             # mass water error to check when leaving fixed point iteration (with seg_fluxes)
             errorsEachC = rhizoTotCAfter_ - ( rhizoTotCBefore_ + (seg_sol_fluxes+ proposed_outer_sol_fluxes+ seg_mucil_fluxes+ proposed_outer_mucil_fluxes)*dt)
             
-            errorsEachC_rel = np.zeros(errorsEachC)
-            errorsEachC_rel[np.where(rhizoTotCAfter_ != 0)] = errorsEachC/rhizoTotCAfter_
-            errorsEachC_rel[np.where((rhizoTotCAfter_ == 0) and (rhizoTotCBefore_ != 0))] = errorsEachC/rhizoTotCBefore_
+            errorsEachC_rel = np.zeros(errorsEachC.shape)
+            idTemp = np.where(rhizoTotCAfter_ != 0)
+            errorsEachC_rel[idTemp] = errorsEachC[idTemp]/rhizoTotCAfter_[idTemp]
+            idTemp =  np.where(np.logical_and((rhizoTotCAfter_ == 0) , (rhizoTotCBefore_ != 0)))
+            errorsEachC_rel[idTemp] = errorsEachC[idTemp] /rhizoTotCBefore_[idTemp] 
             if len(airSegsId)>0:                
                 try:
                     assert (errorsEachC[airSegsId] == 0.).all()