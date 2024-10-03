cb7c802ef7cd21061ba39257ce3a683de663b8e5
Daniel Leitner <d.leitner@fz-juelich.de>
2024-10-03 13:32:23 +0200

    again

--- python/coupled/upscaling/rsync_.sh
+++ python/coupled/upscaling/rsync_.sh
@@ -1,1 +1,2 @@
-rsync -avzP d.leitner@ibg3113.ibg.kfa-juelich.de:/home/d.leitner/DUMUX/dumux-rosi/python/coupled/upscaling/results/ results/
+# run script from local machine in upscaling/ directory
+rsync -avzP d.leitner@ibg3113.ibg.kfa-juelich.de:/home/d.leitner/dumux/dumux-rosi/python/coupled/upscaling/results/ results/
