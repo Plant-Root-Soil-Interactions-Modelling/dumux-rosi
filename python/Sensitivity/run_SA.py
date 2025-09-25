5da680547629c2cd9cd5b2cf3aebafc93a3954aa
Daniel Leitner <d.leitner@fz-juelich.de>
2025-09-25 11:40:00 +0200

    for cluster

--- python/Sensitivity/run_SA.py
+++ python/Sensitivity/run_SA.py
@@ -321,7 +321,7 @@ def simulate_list():
 
     sim_time = 10  # 87.5  # 87.5  # days
     enviro_type = 0
-    start_jobs("file", "", "", enviro_type, sim_time, jobs[0:2], run_local = True)
+    start_jobs("file", "", "", enviro_type, sim_time, jobs[0:2], run_local = False)
     # run_sra.run_soybean_exp(lines[1], enviro_type, sim_time, save_all = False)
 
     # enviro_type = 1
