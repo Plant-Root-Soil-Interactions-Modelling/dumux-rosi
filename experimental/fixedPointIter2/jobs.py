dd09acc9d01e2d7934902813e1143f14cbc400eb
MagdaLa <m.landl@fz-juelich.de>
2025-11-25 13:12:08 +0100

    new jobs file

--- experimental/fixedPointIter2/jobs.py
+++ experimental/fixedPointIter2/jobs.py
@@ -18,8 +18,8 @@ def start_jobs(jobs):
 
     for job in jobs:
         
-        soil_type, res, simMax, exudate = job
-        job_name = soil_type + "_" + res + "_"+ simMax +"_" + exudate
+        soil_type, res, simMax, exudate, decay = job
+        job_name = soil_type + "_" + res + "_"+ simMax +"_" + exudate+"_"+decay
         print(job_name)
         job_file = os.path.join("jobs", job_name + ".sh")
 
@@ -38,7 +38,7 @@ def start_jobs(jobs):
             fh.writelines("cd $HOME/Dumux\n")
             fh.writelines("source cpbenv/bin/activate\n")
             fh.writelines("cd $HOME/Dumux/dumux/dumux-rosi/experimental/fixedPointIter2/scripts\n")
-            fh.writelines("mpiexec -n 1 python3 mainExudate.py {:s} {:s} {:s} {:s}".format(soil_type, res, simMax, exudate))
+            fh.writelines("mpiexec -n 1 python3 mainExudate.py {:s} {:s} {:s} {:s}".format(soil_type, res, simMax, exudate, decay))
         os.system("sbatch {:s}".format(job_file))
 
 
@@ -46,12 +46,13 @@ def make_list():
     jobs = []
 
     # scenario static
-    soil_type = ["loam", "sand"] 
-    res = ["5", "4", "2", "1"] 
+    soil_type = ["loam"] #, "sand"] 
+    res = ["5"] #, "4", "2", "1"] 
     simMax = ['60']
-    exudate = ['True', 'False']
+    exudate = ['True'] #, 'False']
+    decay = ['True', 'False']
 
-    print("Creating", len(soil_type) * len(res) * len(simMax)* len(exudate), "simulations")
+    print("Creating", len(soil_type) * len(res) * len(simMax)* len(exudate)*len(decay), "simulations")
     print()
     
     if not os.path.exists('jobs'):
@@ -61,7 +62,8 @@ def make_list():
         for r in res:
             for m in simMax:
                 for e in exudate:
-                    jobs.append([s, r, m, e])
+                    for d in decay:
+                        jobs.append([s, r, m, e, d])
 
     return jobs
 
