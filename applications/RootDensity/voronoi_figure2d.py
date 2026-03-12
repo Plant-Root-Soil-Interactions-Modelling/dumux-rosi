caa8dfb2c984417553a1a9c02c0e8d86ba1bc51e
Daniel Leitner <d.leitner@fz-juelich.de>
2026-03-12 09:31:26 +0100

    working

--- applications/RootDensity/voronoi_figure2d.py
+++ applications/RootDensity/voronoi_figure2d.py
@@ -1,30 +1,27 @@
-""" figures for the icons (2D)"""
-import sys; sys.path.append("../../build-cmake/cpp/python_binding/"); sys.path.append("../../python/modules/");
-sys.path.append("../../../CPlantBox"); sys.path.append("../../../CPlantBox/src")
+"""vizualisation for the manuscript figure/icons:
+2D tesselation and corresponding ciruclar perirhizal zones
+"""
 
-import plantbox as pb
-from functional.Perirhizal import *
-
-import numpy as np
-import matplotlib.pyplot as plt
 import random
 
+import matplotlib.pyplot as plt
+import numpy as np
+import plantbox as pb
+from plantbox.functional.Perirhizal import *
 from scipy.spatial import ConvexHull, Voronoi
 
 SMALL_SIZE = 16
 MEDIUM_SIZE = 16
 BIGGER_SIZE = 16
-plt.rc('font', size = SMALL_SIZE)  # controls default text sizes
-plt.rc('axes', titlesize = SMALL_SIZE)  # fontsize of the axes title
-plt.rc('axes', labelsize = MEDIUM_SIZE)  # fontsize of the x and y labels
-plt.rc('xtick', labelsize = SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('ytick', labelsize = SMALL_SIZE)  # fontsize of the tick labels
-plt.rc('legend', fontsize = SMALL_SIZE)  # legend fontsize
-plt.rc('figure', titlesize = BIGGER_SIZE)  # fontsize of the figure title
-prop_cycle = plt.rcParams['axes.prop_cycle']
-colors = prop_cycle.by_key()['color']
-
-""" parameters """
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
+prop_cycle = plt.rcParams["axes.prop_cycle"]
+colors = prop_cycle.by_key()["color"]
 
 
 def generate_random_points(num_points, range_):
@@ -47,18 +44,18 @@ def generate_regular_points(xnum_points, ynum_points, xrange, yrange):
 
 
 def flip_(nodes, center, axis):
-    """ flips the nodes around the center according axis """
+    """flips the nodes around the center according axis"""
     # print(nodes.shape)
     # print(center.shape)
     # print(np.ones((nodes.shape[0], 1)).shape)
-    n_ = nodes - np.ones((nodes.shape[0], 1)) @ center
+    n_ = nodes - center
     n_[:, axis] = -n_[:, axis]
-    n_ = n_ + np.ones((nodes.shape[0], 1)) @ center
+    n_ = n_ + center
     return n_
 
 
 def mirror(nodes):
-    """ adds mirrored nodes to the 4 sides """
+    """adds mirrored nodes to the 4 sides"""
     width_ = np.array([[20, 20]])
     center_ = np.array([[0, 0]])
     nodes_surr = nodes
@@ -108,11 +105,11 @@ def vizualize(points):
     for i in range(0, points.shape[0]):
         a = np.sqrt(vol[i] / np.pi)
         # print(vol[i], a * a * np.pi)
-        circ = plt.Circle((points[i, 0], points[i, 1]), a, fill = True, alpha = 0.1, linestyle = 'dotted', color = "red")
+        circ = plt.Circle((points[i, 0], points[i, 1]), a, fill=True, alpha=0.1, linestyle="dotted", color="red")
         axes.add_artist(circ)
 
-    plt.plot(points[:, 0], points[:, 1], 'r*')
-    plt.plot(ver_[:, 0], ver_[:, 1], 'b*')
+    plt.plot(points[:, 0], points[:, 1], "r*")
+    plt.plot(ver_[:, 0], ver_[:, 1], "b*")
     plt.xlim([-10, 10])
     plt.ylim([-10, 10])
 
@@ -120,23 +117,12 @@ def vizualize(points):
 
 
 # Generate 8 random 3D points
-points = generate_random_points(9, 8.)
-print(points)
-# [[ 4.04892037  5.56818163], [ 3.68849136  0.89770249], [ 2.40660187  1.01993938], [-1.87067736  1.70510243], [ 3.91117411 -6.93592641], [-0.9386731  -3.46114548], [ 0.79988892 -0.35288419], [ 2.62590918 -6.30607085]]
-
-# [[-2.90345978  1.88259082]
- # [ 5.14163188 -4.20208878]
- # [-6.28865805 -6.32353276]
- # [ 6.9969697   6.5203306 ]
- # [ 6.34113581 -6.15953657]
- # [-5.17135285  7.72011243]
- # [ 4.86211936  4.5371164 ]
- # [ 6.61824728  1.09913438]
- # [-0.03276024  0.14013829]]
-
+points = generate_random_points(9, 8.0)
 points = generate_regular_points(3, 3, 20, 20)
+print(points)
+
+
 nodes = mirror(points)
 vor = Voronoi(nodes)
 
 vizualize(points)
-
