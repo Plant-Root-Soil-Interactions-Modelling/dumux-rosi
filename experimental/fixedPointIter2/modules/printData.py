fix parallel computation

experimental/fixedPointIter2/modules/printData.py
@@ -187,11 +187,11 @@ def printDiff1d3d(perirhizalModel, s, dt):
         write_file_array("scv_Soil_solute_conc"+str(i+1), 
                          np.array(s.getSolution(i+1)).flatten()* perirhizalModel.bulkDensity_m3 /1e6 , 
                          directory_ =results_dir, fileType = '.csv') 
-    
+
+    TotC = np.array(s.getTotCContent()).flatten()    
+    WaterC = np.array(s.getWaterContent()).flatten()   
     #save TotC and water content of macroscale cells only 
-    if(s.numSoluteComp>0):
-        TotC = np.array(s.getTotCContent()).flatten()    
-        WaterC = np.array(s.getWaterContent()).flatten()   
+    if(s.numSoluteComp>0) and (rank == 0):
         RootCells = perirhizalModel.cellWithRoots # only id of cells with roots
         TotC_macro = np.delete(TotC, RootCells)
         WaterC_macro = np.delete(WaterC, RootCells)

experimental/fixedPointIter2/modules/rhizo_modelsPlant.py
@@ -463,7 +463,6 @@ class RhizoMappedSegments(Perirhizal):#pb.MappedPlant):
         except:
             maxlim1d3d = self.limErr1d3dAbs
             
-        print("Adding manual fix of concentration before the update of the 1d models")
         if len(self.eidx_all_) > 0:
             self.set3Dfrom1Ddata(self.getCellIds())
         
@@ -472,7 +471,7 @@ class RhizoMappedSegments(Perirhizal):#pb.MappedPlant):
         self.finishedUpdate = False
         
         self.eidx = np.concatenate((self.eidx,np.array(self.newEidx, dtype = np.int64)), dtype = np.int64) # 1d model global id on this thread
-        self.eidx_all_ = self._flat0(comm.gather(self.eidx, root=0))# all segs
+        self.eidx_all_ = comm.bcast(self._flat0(comm.gather(self.eidx, root=0)), root=0)# all segs
         
         if rank==0:
             assert len(self.eidx_all_) == len(list(set(list(self.eidx_all_))))