04ee48b0f175986cdc3b82685ca91318e16f0068
Daniel Leitner <d.leitner@fz-juelich.de>
2026-08-11 15:50:20 +0200

    root only examples revised

--- python/roots/vtp2rsml.py
+++ python/roots/vtp2rsml.py
@@ -1,20 +1,20 @@
-import sys; sys.path.append("../modules"); sys.path.append("../../../CPlantBox");  sys.path.append("../../../CPlantBox/src")
-
-import plantbox as pb
-from functional.xylem_flux import XylemFluxPython  # Python hybrid solver
-import rsml.rsml_writer as rsml
-import visualisation.vtk_tools as vt
-import visualisation.vtk_plot as vp
-
 import matplotlib.pyplot as plt
 import numpy as np
+import plantbox as pb
+import plantbox.rsml.rsml_writer as rsml
+import plantbox.visualisation.vtk_plot as vp
+import plantbox.visualisation.vtk_tools as vt
+from plantbox.functional.PlantHydraulicModel import (
+    HydraulicModel_Doussan,
+    HydraulicModel_Meunier,
+)
 
 """ 
 Converts a DuMux output vtp to a RSML
 """
 
-file_in = "test.vtp"  # ../../grids/RootSystem8.vtp"
-file_out = "test.rsml"  # "../../grids/RootSystem8.rsml"
+file_in = "../../grids/RootSystem8.vtp"
+file_out = "../../grids/RootSystem8.rsml"
 
 """ read vtp """
 pd = vt.read_vtp(file_in)
