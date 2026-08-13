9658236f979c5d839da5b0731d0951884ae6d485
Daniel Leitner <d.leitner@fz-juelich.de>
2026-08-13 14:06:53 +0200

    revised

--- python/coupled/C12new/coupled_c12.py
+++ python/coupled/C12new/coupled_c12.py
@@ -1,38 +1,74 @@
-""" 
+"""
 Benchmark M1.2 static root system in soil (root hydraulics with Doussan or Meunier using the classic sink)
+
+Fro age dependent conductivities:
+*) are very unstable, work better for larger spatial resolutions only (e.g. [4, 4, 15]), small temporal dt = 60 s
+*) cache = False for Meunier, otherwise wrong
 
 run times (for 7.1 days)
 Meunier               195s
-Meunier cached        200s (slower than direct...) 
+Meunier cached        200s (slower than direct...)
 Doussan               213
 Doussan cached        186
 
 """
-import sys; sys.path.append("../../modules"); sys.path.append("../../../build-cmake/cpp/python_binding/");
-sys.path.append("../../../../CPlantBox");  sys.path.append("../../../../CPlantBox/src")
 
-import plantbox as pb
-from functional.PlantHydraulicModel import HydraulicModel_Doussan
-from functional.PlantHydraulicModel import HydraulicModel_Meunier
-from functional.PlantHydraulicParameters import PlantHydraulicParameters
-import functional.van_genuchten as vg
-from functional.root_conductivities import *
-import rsml.rsml_reader as rsml
-import visualisation.vtk_plot as vp
-
-from rosi_richards import RichardsSPSSORC as RichardsSP  # C++ part (Dumux binding)
-from richards import RichardsWrapper  # Python part
-from rhizo_models import plot_transpiration
-
-import numpy as np
-import matplotlib.pyplot as plt
 import timeit
-from mpi4py import MPI; comm = MPI.COMM_WORLD; rank = comm.Get_rank()
+
+import matplotlib.pyplot as plt
+import numpy as np
+import plantbox as pb
+import plantbox.functional.van_genuchten as vg
+import plantbox.rsml.rsml_reader as rsml
+import plantbox.visualisation.vtk_plot as vp
+from mpi4py import MPI
+from plantbox.functional.PlantHydraulicModel import (
+    HydraulicModel_Doussan,
+    HydraulicModel_Meunier,
+)
+from plantbox.functional.PlantHydraulicParameters import PlantHydraulicParameters
+from plantbox.functional.root_conductivities import *
+from rosi.richards import RichardsWrapper  # Python part
+from rosi.rosi_richards import RichardsSPSSORC as RichardsSP  # C++ part (Dumux binding)
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+
+def plot_transpiration(t, soil_uptake, root_uptake, potential_trans, title=""):
+    """plots potential and actual transpiration over time
+
+    depending on discretisation soil_uptake and root_uptake might differ
+
+    @param t                  times [day]
+    @param soil_uptake        actual transpiration [cm3/day] of soil
+    @param root_uptake        actual transpiration [cm3/day] according to root model
+    @param potential_trans    function in t stating the potential transpiration [cm3/day]
+    """
+    fig, ax1 = plt.subplots()
+    ax1.plot(t, [potential_trans(t_) for t_ in t], "k", label="potential transpiration")  # potential transpiration
+    ax1.plot(t, -np.array(soil_uptake), "g", label="soil uptake")  # actual transpiration  according to soil model
+    ax1.plot(t, -np.array(root_uptake), "r:", label="root system uptake")  # actual transpiration according root model
+    ax1.set_xlabel("Time [d]")
+    ax1.set_ylabel("Transpiration $[cm^3 d^{-1}]$")
+    ax2 = ax1.twinx()
+    dt = np.diff(t)
+    so = np.array(soil_uptake)
+    cum_transpiration = np.cumsum(-np.multiply(so[:-1], dt))
+    ax2.plot(t[1:], cum_transpiration, "c--")  # cumulative transpiration (neumann)
+    ax2.set_ylabel("Cumulative soil uptake $[cm^3]$")
+    print("Cumulative soil uptake", cum_transpiration[-1], "[cm^3]")
+    fig.legend()
+    plt.title(title)
+    if title:
+        plt.savefig(title + ".png")
+    plt.show()
+
 
 """ Parameters """
-min_b = [-4., -4., -15.]
-max_b = [4., 4., 0.]
-cell_number = [8, 8, 15] # [8, 8, 15]
+min_b = [-4.0, -4.0, -15.0]
+max_b = [4.0, 4.0, 0.0]
+cell_number = [4, 4, 15]  # [8, 8, 15]
 periodic = False
 path = ""
 fname = "../../../grids/RootSystem8.rsml"
@@ -48,9 +84,9 @@ initial = -659.8 + 7.5  # -659.8
 trans = 6.4  # cm3 /day (sinusoidal)
 wilting_point = -15000  # cm
 
-sim_time = 7.1  # 7.1  # [day] for task b
-age_dependent = False  # conductivities
-dt = 120. / (24 * 3600)  # [days] Time step must be very small
+sim_time = 3.1  # 7.1  # [day] for task b
+age_dependent = True  # conductivities
+dt = 60.0 / (24 * 3600)  # [days] Time step must be very small
 skip = 10
 
 """ Initialize macroscopic soil model """
@@ -64,24 +100,18 @@ s.setBotBC("noFlux")
 s.setVGParameters([soil])
 s.setParameter("Newton.EnableChop", "True")
 s.setParameter("Newton.EnableAbsoluteResidualCriterion", "True")
-s.setParameter("Soil.SourceSlope", "100")  # turns regularisation of the source term on, will change the shape of actual transpiration...
+s.setParameter("Soil.SourceSlope", "1000")  # turns regularisation of the source term on, will change the shape of actual transpiration...
 s.initializeProblem()
 s.setCriticalPressure(wilting_point)
 
 """ Initialize root hydraulic model (a) or (b)"""
-sinusoidal = HydraulicModel_Doussan.sinusoidal  # rename
+sinusoidal = HydraulicModel_Meunier.sinusoidal  # rename
 params = PlantHydraulicParameters()
 init_conductivities(params, age_dependent)
 
-r = HydraulicModel_Meunier(fname, params, cached = True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
+r = HydraulicModel_Doussan(fname, params, cached=True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
 
-r.ms.setRectangularGrid(pb.Vector3d(min_b[0], min_b[1], min_b[2]), pb.Vector3d(max_b[0], max_b[1], max_b[2]),
-                        pb.Vector3d(cell_number[0], cell_number[1], cell_number[2]), cut = False)  # cutting
-
-""" Coupling (map indices) """
-picker = lambda x, y, z: s.pick([x, y, z])
-cci = picker(r.ms.nodes[0].x, r.ms.nodes[0].y, r.ms.nodes[0].z)  # collar cell index
-r.ms.setSoilGrid(picker)  # maps segment
+r.ms.setRectangularGrid(pb.Vector3d(min_b[0], min_b[1], min_b[2]), pb.Vector3d(max_b[0], max_b[1], max_b[2]), pb.Vector3d(cell_number[0], cell_number[1], cell_number[2]), cut=False)  # cutting
 
 """ sanity checks """
 # r.params.plot_conductivities()
@@ -96,47 +126,46 @@ x_, y_, z_ = [], [], []
 sink1d = []
 sx = s.getSolutionHead()  # inital condition, solverbase.py
 N = round(sim_time / dt)
-t = 0.
+t = 0.0
 
 
 for i in range(0, N):
 
     if rank == 0:  # Root part is not parallel
-        if i == 0: 
-            rx = r.solve(rs_age, -trans * sinusoidal(t), sx, cells = True)
-        rx = r.solve_again(rs_age + t, -trans * sinusoidal(t), sx, cells = True)
+        if i == 0:
+            rx = r.solve(rs_age, -trans * sinusoidal(t), sx, cells=True)
+        rx = r.solve_again(rs_age + t, -trans * sinusoidal(t), sx, cells=True)
         fluxes = r.soil_fluxes(rs_age + t, rx, sx)
 
     else:
         fluxes = None
 
-    fluxes = comm.bcast(fluxes, root = 0)  # Soil part runs parallel
+    fluxes = comm.bcast(fluxes, root=0)  # Soil part runs parallel
 
     water = s.getWaterVolume()
 
     s.setSource(fluxes.copy())
     s.solve(dt)
-    
+
     if rank == 0:
         old_sx = sx.copy()
-        
+
     sx = s.getSolutionHead()
 
     soil_water = (s.getWaterVolume() - water) / dt  # since no-flux bc everywhere, change in water volume should equal transpirational flux
 
     if rank == 0 and i % skip == 0:
         x_.append(t)
-        sum_flux = 0.
+        sum_flux = 0.0
         for f in fluxes.values():
             sum_flux += f
-        act_trans = r.get_transpiration(rs_age + t, rx, old_sx, cells = True)
+        act_trans = r.get_transpiration(rs_age + t, rx, old_sx, cells=True)
         print("Summed fluxes ", sum_flux, "= collar flux", act_trans, "= prescribed", -trans * sinusoidal(t))
         # y_.append(soil_water)  # cm3/day (soil uptake)
         y_.append(sum_flux)  # cm3/day (root system uptake)
         z_.append(act_trans)  # cm3/day
-        n = round(float(i) / float(N) * 100.)
-        print("[" + ''.join(["*"]) * n + ''.join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n"
-              .format(np.min(sx), np.max(sx), np.min(rx), np.max(rx), s.simTime, rx[0]))
+        n = round(float(i) / float(N) * 100.0)
+        print("[" + "".join(["*"]) * n + "".join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n".format(np.min(sx), np.max(sx), np.min(rx), np.max(rx), s.simTime, rx[0]))
 
         # """ Additional sink plot """
         # if i % 60 == 0:  # every 6h
@@ -153,16 +182,15 @@ s.writeDumuxVTK(name)
 
 """ Plot """
 if rank == 0:
-    print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
-    
+    print("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
+
     # vp.plot_roots_and_soil(r.ms, "pressure head", rx, s, periodic, min_b, max_b, cell_number, name)  # VTK vizualisation
-    
+
     plot_transpiration(x_, y_, z_, lambda t: trans * sinusoidal(t))
-    # TODO getWaterVolume() does not work with MPI 
-    
+    # TODO getWaterVolume() does not work with MPI
+
     # np.savetxt(name, np.vstack((x_, -np.array(y_))), delimiter = ';')
     # sink1d = np.array(sink1d)
     # np.save("sink1d", sink1d)
     # print(sink1d.shape)
     # print(sink1d[-1])
-
--- python/coupled/C12new/coupled_c12_.py
+++ python/coupled/C12new/coupled_c12_.py
@@ -1,29 +1,61 @@
-""" 
-couple_c12 without mpi 
 """
-import sys; sys.path.append("../../modules"); sys.path.append("../../../build-cmake/cpp/python_binding/");
-sys.path.append("../../../../CPlantBox");  sys.path.append("../../../../CPlantBox/src")
+couple_c12 without mpi
+"""
 
-import plantbox as pb
-from functional.PlantHydraulicModel import HydraulicModel_Meunier
-from functional.PlantHydraulicParameters import PlantHydraulicParameters
-import functional.van_genuchten as vg
-from functional.root_conductivities import *
-import rsml.rsml_reader as rsml
-import visualisation.vtk_plot as vp
-
-from rosi_richards import RichardsSP  # C++ part (Dumux binding)
-from richards import RichardsWrapper  # Python part
-from rhizo_models import plot_transpiration
-
-import numpy as np
-import matplotlib.pyplot as plt
 import timeit
 
+import matplotlib.pyplot as plt
+import numpy as np
+import plantbox as pb
+import plantbox.functional.van_genuchten as vg
+import plantbox.rsml.rsml_reader as rsml
+import plantbox.visualisation.vtk_plot as vp
+from plantbox.functional.PlantHydraulicModel import (
+    HydraulicModel_Doussan,
+    HydraulicModel_Meunier,
+)
+from plantbox.functional.PlantHydraulicParameters import PlantHydraulicParameters
+from plantbox.functional.root_conductivities import *
+from rosi.richards import RichardsWrapper  # Python part
+
+# from rosi.rosi_richards import RichardsSP  # C++ part (Dumux binding)
+from rosi.rosi_richards import RichardsSPSSORC as RichardsSP  # C++ part (Dumux binding)
+
+
+def plot_transpiration(t, soil_uptake, root_uptake, potential_trans, title=""):
+    """plots potential and actual transpiration over time
+
+    depending on discretisation soil_uptake and root_uptake might differ
+
+    @param t                  times [day]
+    @param soil_uptake        actual transpiration [cm3/day] of soil
+    @param root_uptake        actual transpiration [cm3/day] according to root model
+    @param potential_trans    function in t stating the potential transpiration [cm3/day]
+    """
+    fig, ax1 = plt.subplots()
+    ax1.plot(t, [potential_trans(t_) for t_ in t], "k", label="potential transpiration")  # potential transpiration
+    ax1.plot(t, -np.array(soil_uptake), "g", label="soil uptake")  # actual transpiration  according to soil model
+    ax1.plot(t, -np.array(root_uptake), "r:", label="root system uptake")  # actual transpiration according root model
+    ax1.set_xlabel("Time [d]")
+    ax1.set_ylabel("Transpiration $[cm^3 d^{-1}]$")
+    ax2 = ax1.twinx()
+    dt = np.diff(t)
+    so = np.array(soil_uptake)
+    cum_transpiration = np.cumsum(-np.multiply(so[:-1], dt))
+    ax2.plot(t[1:], cum_transpiration, "c--")  # cumulative transpiration (neumann)
+    ax2.set_ylabel("Cumulative soil uptake $[cm^3]$")
+    print("Cumulative soil uptake", cum_transpiration[-1], "[cm^3]")
+    fig.legend()
+    plt.title(title)
+    if title:
+        plt.savefig(title + ".png")
+    plt.show()
+
+
 """ Parameters """
-min_b = [-4., -4., -15.]
-max_b = [4., 4., 0.]
-cell_number = [8, 8, 15]
+min_b = [-4.0, -4.0, -15.0]
+max_b = [4.0, 4.0, 0.0]
+cell_number = [4, 4, 15]
 periodic = False
 path = ""
 fname = "../../../grids/RootSystem8.rsml"
@@ -39,9 +71,9 @@ initial = -659.8 + 7.5  # -659.8
 trans = 6.4  # cm3 /day (sinusoidal)
 wilting_point = -15000  # cm
 
-sim_time = 7.1  # 7.1  # [day] for task b
-age_dependent = False  # conductivities
-dt = 120. / (24 * 3600)  # [days] Time step must be very small
+sim_time = 3.1  # 7.1  # 7.1  # [day] for task b
+age_dependent = True  # conductivities
+dt = 60.0 / (24 * 3600)  # [days] Time step must be very small
 skip = 10
 
 """ Initialize macroscopic soil model """
@@ -55,7 +87,7 @@ s.setBotBC("noFlux")
 s.setVGParameters([soil])
 s.setParameter("Newton.EnableChop", "True")
 s.setParameter("Newton.EnableAbsoluteResidualCriterion", "True")
-s.setParameter("Soil.SourceSlope", "100")  # turns regularisation of the source term on, will change the shape of actual transpiration...
+s.setParameter("Soil.SourceSlope", "1000")  # turns regularisation of the source term on, will change the shape of actual transpiration...
 s.initializeProblem()
 s.setCriticalPressure(wilting_point)
 
@@ -63,51 +95,47 @@ s.setCriticalPressure(wilting_point)
 sinusoidal = HydraulicModel_Meunier.sinusoidal  # rename
 params = PlantHydraulicParameters()
 init_conductivities(params, age_dependent)
+# params.plot_conductivities()
 
-r = HydraulicModel_Meunier(fname, params, cached = True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
-
-r.ms.setRectangularGrid(pb.Vector3d(min_b[0], min_b[1], min_b[2]), pb.Vector3d(max_b[0], max_b[1], max_b[2]),
-                        pb.Vector3d(cell_number[0], cell_number[1], cell_number[2]), cut = False)  
+r = HydraulicModel_Doussan(fname, params, cached=True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
+r.ms.setRectangularGrid(pb.Vector3d(min_b[0], min_b[1], min_b[2]), pb.Vector3d(max_b[0], max_b[1], max_b[2]), pb.Vector3d(cell_number[0], cell_number[1], cell_number[2]), cut=False)
 
 """ Numerical solution (a) """
 start_time = timeit.default_timer()
 
 x_, y_, z_ = [], [], []
 N = round(sim_time / dt)
-t = 0.
+t = 0.0
 rs_age = np.max(r.get_ages())
 
-sx = s.getSolutionHead()  
-rx = r.solve(rs_age, -trans * sinusoidal(t), sx, cells = True)
+sx = s.getSolutionHead()
+rx = r.solve(rs_age, -trans * sinusoidal(t), sx, cells=True)
 
 for i in range(0, N):
 
-    rx = r.solve_again(rs_age + t, -trans * sinusoidal(t), sx, cells = True)
-    
+    rx = r.solve_again(rs_age + t, -trans * sinusoidal(t), sx, cells=True)
+
     fluxes = r.soil_fluxes(rs_age + t, rx, sx)
     s.setSource(fluxes.copy())
     s.solve(dt)
-    
+
     old_sx = sx.copy()
     sx = s.getSolutionHead()
 
     if i % skip == 0:
         x_.append(t)
-        sum_flux = 0.
+        sum_flux = 0.0
         for f in fluxes.values():
             sum_flux += f
-        act_trans = r.get_transpiration(rs_age + t, rx, old_sx, cells = True)
+        act_trans = r.get_transpiration(rs_age + t, rx, old_sx, cells=True)
         print("Summed fluxes ", sum_flux, "= collar flux", act_trans, "= prescribed", -trans * sinusoidal(t))
         y_.append(sum_flux)  # cm3/day (root system uptake)
         z_.append(act_trans)  # cm3/day
-        n = round(float(i) / float(N) * 100.)
-        print("[" + ''.join(["*"]) * n + ''.join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n"
-              .format(np.min(sx), np.max(sx), np.min(rx), np.max(rx), s.simTime, rx[0]))
+        n = round(float(i) / float(N) * 100.0)
+        print("[" + "".join(["*"]) * n + "".join([" "]) * (100 - n) + "], soil [{:g}, {:g}] cm, root [{:g}, {:g}] cm, {:g} days {:g}\n".format(np.min(sx), np.max(sx), np.min(rx), np.max(rx), s.simTime, rx[0]))
 
     t += dt
 
 """ Plot """
-print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
+print("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
 plot_transpiration(x_, y_, z_, lambda t: trans * sinusoidal(t))
-
-
